3eaeb216727ca6f2a020e834a1e90a3a5d043a5e
pm47 <spam4420@gmail.com>
2015-08-20 12:29:28 +0200

    first dummy impl using FSM

---
+++ eclair-demo/src/main/scala/fr/acinq/eclair/Boot.scala
@@ -0,0 +1,40 @@
+package fr.acinq.eclair
+
+import akka.actor.{Props, ActorSystem}
+import com.google.protobuf.ByteString
+import lightning._
+import lightning.locktime.Locktime._
+import lightning.open_channel.anchor_offer.WILL_CREATE_ANCHOR
+
+/**
+ * Created by PM on 20/08/2015.
+ */
+object Boot extends App {
+
+  val system = ActorSystem()
+
+  val node = system.actorOf(Props(classOf[Node]))
+
+  node ! open_channel(
+    delay = locktime(Blocks(1)),
+    revocationHash = sha256_hash(1, 1, 1, 1),
+    commitKey = bitcoin_pubkey(ByteString.copyFromUtf8("")),
+    finalKey = bitcoin_pubkey(ByteString.copyFromUtf8("")),
+    anch = WILL_CREATE_ANCHOR,
+    minDepth = Some(2),
+    commitmentFee = 100)
+
+  node ! open_anchor(
+    txid = sha256_hash(1, 1, 1, 1),
+    outputIndex = 1,
+    amount = 100000000L,
+    commitSig = signature(1, 1, 1, 1, 0, 0, 0, 0))
+
+  node ! TxConfirmed(1)
+  node ! TxConfirmed(2)
+
+  node ! open_complete(
+    blockid = Some(sha256_hash(1, 1, 1, 1))
+  )
+
+}
---
+++ eclair-demo/src/main/scala/fr/acinq/eclair/Node.scala
@@ -0,0 +1,65 @@
+package fr.acinq.eclair
+
+import akka.actor.{LoggingFSM, FSM}
+import lightning._
+import lightning.open_channel.anchor_offer.WILL_CREATE_ANCHOR
+
+/**
+ * Created by PM on 20/08/2015.
+ */
+
+sealed trait State
+case object OPEN_WAIT_FOR_OPEN_NOANCHOR extends State
+case object OPEN_WAIT_FOR_ANCHOR extends State
+case object OPEN_WAITING extends State
+case object OPEN_WAIT_FOR_COMPLETE extends State
+case object NORMAL extends State
+
+sealed trait Data
+case object Uninitialized extends Data
+final case class ChannelParams(revocationHash: sha256_hash, minDepth: Int) extends Data
+
+
+//blockchain events
+sealed trait BlockchainEvent
+final case class TxConfirmed(confirmations: Int)
+
+class Node extends LoggingFSM[State, Data] {
+
+  startWith(OPEN_WAIT_FOR_OPEN_NOANCHOR, Uninitialized)
+
+  when(OPEN_WAIT_FOR_OPEN_NOANCHOR) {
+    case Event(open_channel(delay, revocationHash, commitKey, finalKey, WILL_CREATE_ANCHOR, minDepth, commitmentFee), Uninitialized) =>
+      // Nothing to do here, we wait for the anchor
+      goto(OPEN_WAIT_FOR_ANCHOR) using ChannelParams(revocationHash, minDepth.get) // minDepth has a default value in the .proto
+  }
+
+  when(OPEN_WAIT_FOR_ANCHOR) {
+    case Event(open_anchor(txid, outputIndex, amount: Long, commitSig), params@ChannelParams(revocationHash, _)) =>
+      // TODO : sign commitment tx
+      // TODO : reply with commit tx sig
+      // TODO : register for confirmations of anchor tx on the bitcoin network
+      goto(OPEN_WAITING) using params
+  }
+
+  when(OPEN_WAITING) {
+    case Event(TxConfirmed(confirmations), params@ChannelParams(_, minDepth)) if confirmations < minDepth =>
+      log.info(s"got $confirmations confirmations for anchor tx")
+      stay using params
+    case Event(TxConfirmed(confirmations), params@ChannelParams(_, minDepth)) if confirmations >= minDepth =>
+      log.info(s"got $confirmations confirmations for anchor tx, minDepth reached")
+      //TODO : send open complete message
+      goto(OPEN_WAIT_FOR_COMPLETE)
+  }
+
+  when(OPEN_WAIT_FOR_COMPLETE) {
+    case Event(open_complete(blockid_opt), _) =>
+      goto(NORMAL)
+  }
+
+  when(NORMAL) {
+    case Event(_, _) =>
+      stay
+  }
+
+}
