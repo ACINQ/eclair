8bdd36bffa3defdbd10ba2c23be21764a9ae9dc8
pm47 <spam4420@gmail.com>
2016-01-27 21:15:17 +0100

    added a ChannelState test

--- eclair-demo/src/test/scala/fr/acinq/eclair/ChannelStateSpec.scala
+++ eclair-demo/src/test/scala/fr/acinq/eclair/ChannelStateSpec.scala
@@ -14,7 +14,7 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class ChannelStateSpec extends FunSuite {
 
-  test("fee management") {
+  test("fee management send") {
     val state_0 = ChannelState(
       us = ChannelOneSide(pay_msat = 950000000, fee_msat = 50000000, htlcs = Seq()),
       them = ChannelOneSide(pay_msat = 0, fee_msat = 0, htlcs = Seq())
@@ -33,6 +33,28 @@ class ChannelStateSpec extends FunSuite {
     assert(state_2 === ChannelState(
       us = ChannelOneSide(pay_msat = 875000000, fee_msat = 25000000, htlcs = Seq()),
       them = ChannelOneSide(pay_msat = 75000000, fee_msat = 25000000, htlcs = Seq())
+    ))
+  }
+
+  test("fee management receive") {
+    val state_0 = ChannelState(
+      us = ChannelOneSide(pay_msat = 0, fee_msat = 0, htlcs = Seq()),
+      them = ChannelOneSide(pay_msat = 950000000, fee_msat = 50000000, htlcs = Seq())
+    )
+
+    val r = sha256_hash(7, 7, 7, 7)
+    val rHash = Crypto.sha256(r)
+    val htlc = update_add_htlc(rHash, 2000000, rHash, locktime(Blocks(1)))
+    val state_1 = state_0.htlc_receive(htlc)
+    assert(state_1 === ChannelState(
+      us = ChannelOneSide(pay_msat = 0, fee_msat = 0, htlcs = Seq(htlc)),
+      them = ChannelOneSide(pay_msat = 948000000, fee_msat = 50000000, htlcs = Seq())
+    ))
+
+    val state_2 = state_1.htlc_fulfill(r)
+    assert(state_2 === ChannelState(
+      us = ChannelOneSide(pay_msat = 0, fee_msat = 2000000, htlcs = Seq()),
+      them = ChannelOneSide(pay_msat = 950000000, fee_msat = 48000000, htlcs = Seq())
     ))
   }
 
