0e8614b9f5de10eb72a6d27723770a41626b1af7
sstone <samuel.stone@hotmail.co.za>
2016-04-18 14:45:28 +0200

    implement AEAD using chacha20-poly1305

---
+++ eclair-demo/src/main/java/fr/acinq/eclair/crypto/Poly3105.java
@@ -0,0 +1,142 @@
+package fr.acinq.eclair.crypto;
+
+public class Poly3105 {
+    final int CRYPTO_BYTES = 16;
+    final int CRYPTO_KEYBYTES = 32;
+
+    static final int[] minusp = {5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 252};
+
+    static void add(int[] h, int[] c)
+    {
+        int j;
+        int u = 0;
+
+        for (j = 0; j < 17; ++j)
+        {
+            u += h[j] + c[j];
+            h[j] = u & 255;
+            u >>>= 8;
+        }
+    }
+
+    static void squeeze(int[] h)
+    {
+        int u = 0;
+
+        for (int j = 0; j < 16; ++j)
+        {
+            u += h[j];
+            h[j] = u & 255;
+            u >>>= 8;
+        }
+
+        u += h[16];
+        h[16] = u & 3;
+        u = 5 * (u >>> 2);
+
+        for (int j = 0; j < 16; ++j)
+        {
+            u += h[j];
+            h[j] = u & 255;
+            u >>>= 8;
+        }
+
+        u += h[16];
+        h[16] = u;
+    }
+
+    static void freeze(int[] h)
+    {
+        int[] horig = new int[17];
+
+        for (int j = 0; j < 17; ++j)
+            horig[j] = h[j];
+
+        add(h, minusp);
+
+        int negative = (int)(-(h[16] >>> 7));
+
+        for (int j = 0; j < 17; ++j)
+            h[j] ^= negative & (horig[j] ^ h[j]);
+    }
+
+    static void mulmod(int[] h, int[] r)
+    {
+        int[] hr = new int[17];
+
+        for (int i = 0; i < 17; ++i)
+        {
+            int u = 0;
+
+            for (int j = 0; j <= i; ++j)
+                u += h[j] * r[i - j];
+
+            for (int j = i + 1; j < 17; ++j)
+                u += 320 * h[j] * r[i + 17 - j];
+
+            hr[i] = u;
+        }
+
+        for (int i = 0; i < 17; ++i)
+            h[i] = hr[i];
+
+        squeeze(h);
+    }
+
+    public static int crypto_onetimeauth(byte[] outv, int outvoffset, byte[] inv, int invoffset, long inlen, byte[] k)
+    {
+        int j;
+        int[] r = new int[17];
+        int[] h = new int[17];
+        int[] c = new int[17];
+
+        r[0] = k[0] & 0xFF;
+        r[1] = k[1] & 0xFF;
+        r[2] = k[2] & 0xFF;
+        r[3] = k[3] & 15;
+        r[4] = k[4] & 252;
+        r[5] = k[5] & 0xFF;
+        r[6] = k[6] & 0xFF;
+        r[7] = k[7] & 15;
+        r[8] = k[8] & 252;
+        r[9] = k[9] & 0xFF;
+        r[10] = k[10] & 0xFF;
+        r[11] = k[11] & 15;
+        r[12] = k[12] & 252;
+        r[13] = k[13] & 0xFF;
+        r[14] = k[14] & 0xFF;
+        r[15] = k[15] & 15;
+        r[16] = 0;
+
+        for (j = 0; j < 17; ++j)
+            h[j] = 0;
+
+        while (inlen > 0)
+        {
+            for (j = 0; j < 17; ++j)
+                c[j] = 0;
+
+            for (j = 0; (j < 16) && (j < inlen); ++j)
+                c[j] = inv[invoffset + j]&0xff;
+
+            c[j] = 1;
+            invoffset += j;
+            inlen -= j;
+            add(h, c);
+            mulmod(h, r);
+        }
+
+        freeze(h);
+
+        for (j = 0; j < 16; ++j)
+            c[j] = k[j + 16] & 0xFF;
+
+        c[16] = 0;
+        add(h, c);
+
+        for (j = 0; j < 16; ++j)
+            outv[j + outvoffset] = (byte)h[j];
+
+        return 0;
+    }
+}
---
+++ eclair-demo/src/test/scala/fr/acinq/eclair/crypto/LightningCryptoSpec.scala
@@ -0,0 +1,58 @@
+package fr.acinq.eclair.crypto
+
+import fr.acinq.bitcoin.BinaryData
+import fr.acinq.eclair.crypto.LightningCrypto.AeadChacha20Poly1305
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+
+@RunWith(classOf[JUnitRunner])
+class LightningCryptoSpec extends FunSuite {
+  test("Poly1305") {
+    val key: BinaryData = "85d6be7857556d337f4452fe42d506a80103808afb0db2fd4abff6af4149f51b"
+    val data: BinaryData = "Cryptographic Forum Research Group".getBytes("UTF-8")
+
+    val bar = new Array[Byte](16)
+    val mac = LightningCrypto.poly1305(key, data)
+    assert(mac === BinaryData("a8061dc1305136c6c22b8baf0c0127a9"))
+  }
+
+  test("Chacha20") {
+    val plaintext: BinaryData =  "Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it.".getBytes
+    val key: BinaryData = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+    val nonce: BinaryData = "0000004a00000000"
+
+    val ciphertext = LightningCrypto.chacha20Encrypt(plaintext, key, nonce, 1)
+    assert(ciphertext == BinaryData("6e2e359a2568f98041ba0728dd0d6981e97e7aec1d4360c20a27afccfd9fae0bf91b65c5524733ab8f593dabcd62b3571639d624e65152ab8f530c359f0861d807ca0dbf500d6a6156a38e088a22b65e52bc514d16ccf806818ce91ab77937365af90bbf74a35be6b40b8eedf2785e42874d"))
+
+    assert(LightningCrypto.chacha20Decrypt(ciphertext, key, nonce, 1) == plaintext)
+  }
+
+  test("Chacha20Poly1305 key generation test") {
+    // see https://tools.ietf.org/html/rfc7539#section-2.6.2
+    val key: BinaryData = "808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f"
+    val nonce: BinaryData = "0001020304050607"
+    assert(LightningCrypto.poly1305KenGen(key, nonce) == BinaryData("8ad5a08b905f81cc815040274ab29471a833b637e3fd0da508dbb8e2fdd1a646"))
+  }
+
+  test("AeadChacha20Poly1305 encryption") {
+    val plaintext = BinaryData("010203040506070809")
+    val key = BinaryData("000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f")
+    val nonce = BinaryData("0000004a00000000")
+    val aad = BinaryData("0000004a00000000")
+
+    val (ciphertext, mac) = AeadChacha20Poly1305.encrypt(key, nonce, plaintext, aad)
+    assert(ciphertext == BinaryData("234d52f7451ddee926"))
+    assert(mac == BinaryData("00ef1c5c90d980b565b1ef3fb0d1c062"))
+
+    val check = AeadChacha20Poly1305.decrypt(key, nonce, ciphertext, aad, mac)
+    assert(check == plaintext)
+
+    intercept[AssertionError] {
+      val mac1 = mac
+      mac1(0) = 0xff.toByte
+      AeadChacha20Poly1305.decrypt(key, nonce, ciphertext, aad, mac1)
+    }
+  }
+}
