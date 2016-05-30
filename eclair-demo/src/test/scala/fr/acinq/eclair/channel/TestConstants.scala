f26718d50ca1762a43ac6a81530907cc82d82b1a
pm47 <spam4420@gmail.com>
2016-05-30 18:09:09 +0200

    changed test constants

--- eclair-demo/src/test/scala/fr/acinq/eclair/channel/TestConstants.scala
+++ eclair-demo/src/test/scala/fr/acinq/eclair/channel/TestConstants.scala
@@ -9,20 +9,20 @@ import lightning.locktime.Locktime.Blocks
   * Created by PM on 26/04/2016.
   */
 object TestConstants {
-  val anchorAmount = 100100000L
+  val anchorAmount = 1000000L
 
   // Alice is funder, Bob is not
 
   object Alice {
     val (Base58.Prefix.SecretKeyTestnet, commitPrivKey) = Base58Check.decode("cQPmcNr6pwBQPyGfab3SksE9nTCtx9ism9T4dkS9dETNU2KKtJHk")
     val (Base58.Prefix.SecretKeyTestnet, finalPrivKey) = Base58Check.decode("cUrAtLtV7GGddqdkhUxnbZVDWGJBTducpPoon3eKp9Vnr1zxs6BG")
-    val channelParams = OurChannelParams(locktime(Blocks(10)), commitPrivKey, finalPrivKey, 1, 100000, "alice-seed".getBytes(), Some(anchorAmount))
+    val channelParams = OurChannelParams(locktime(Blocks(10)), commitPrivKey, finalPrivKey, 1, 10000, "alice-seed".getBytes(), Some(anchorAmount))
   }
 
   object Bob {
     val (Base58.Prefix.SecretKeyTestnet, commitPrivKey) = Base58Check.decode("cSUwLtdZ2tht9ZmHhdQue48pfe7tY2GT2TGWJDtjoZgo6FHrubGk")
     val (Base58.Prefix.SecretKeyTestnet, finalPrivKey) = Base58Check.decode("cPR7ZgXpUaDPA3GwGceMDS5pfnSm955yvks3yELf3wMJwegsdGTg")
-    val channelParams = OurChannelParams(locktime(Blocks(10)), commitPrivKey, finalPrivKey, 2, 100000, "bob-seed".getBytes(), None)
+    val channelParams = OurChannelParams(locktime(Blocks(10)), commitPrivKey, finalPrivKey, 2, 10000, "bob-seed".getBytes(), None)
   }
 
 }
