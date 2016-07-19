64638e27bbfa28a7689b7b0ef8b2b66588a23429
pm47 <spam4420@gmail.com>
2016-07-19 11:48:34 +0200

    added CORS headers

--- eclair-demo/src/main/scala/fr/acinq/eclair/api/Service.scala
+++ eclair-demo/src/main/scala/fr/acinq/eclair/api/Service.scala
@@ -3,7 +3,8 @@ package fr.acinq.eclair.api
 import java.net.InetSocketAddress
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.model.{HttpEntity, StatusCodes, ContentTypes, HttpResponse}
+import akka.http.scaladsl.model.headers.RawHeader
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.util.Timeout
 import akka.http.scaladsl.server.Directives._
 import fr.acinq.bitcoin.BinaryData
@@ -29,7 +30,9 @@ import fr.acinq.eclair.channel.Register.ListChannels
   */
 
 case class JsonRPCBody(jsonrpc: String = "1.0", id: String = "scala-client", method: String, params: Seq[JValue])
+
 case class Error(code: Int, message: String)
+
 case class JsonRPCRes(result: AnyRef, error: Option[Error], id: String)
 
 //TODO : use Json4sSupport ?
@@ -40,7 +43,9 @@ trait Service extends Logging {
   implicit val formats = org.json4s.DefaultFormats + new BinaryDataSerializer + new StateSerializer + new Sha256Serializer
   implicit val timeout = Timeout(30 seconds)
 
-  def connect(addr: InetSocketAddress, amount: Long): Unit // amount in satoshis
+  def connect(addr: InetSocketAddress, amount: Long): Unit
+
+  // amount in satoshis
   def register: ActorRef
 
   def sendCommand(channel_id: String, cmd: Command): Future[String] = {
@@ -50,57 +55,65 @@ trait Service extends Logging {
     })
   }
 
+  val customHeaders = RawHeader("Access-Control-Allow-Origin", "*") ::
+    RawHeader("Access-Control-Allow-Headers", "Content-Type") ::
+    RawHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS") ::
+    RawHeader("Cache-control", "public, no-store, max-age=0") ::
+    RawHeader("Access-Control-Allow-Headers", "x-requested-with") :: Nil
+
   val route =
     pathSingleSlash {
-      post {
-        entity(as[String]) {
-          body =>
-            val json = parse(body).extract[JsonRPCBody]
-            val f_res: Future[AnyRef] = json match {
-              case JsonRPCBody(_, _, "connect", JString(host) :: JInt(port) :: JInt(anchor_amount) :: Nil) =>
-                connect(new InetSocketAddress(host, port.toInt), anchor_amount.toLong)
-                Future.successful("")
-              case JsonRPCBody(_, _, "list", _) =>
-                (register ? ListChannels).mapTo[Iterable[ActorRef]]
-                  .flatMap(l => Future.sequence(l.map(c => c ? CMD_GETINFO)))
-              case JsonRPCBody(_, _, "addhtlc", JInt(amount) :: JString(rhash) :: JInt(expiry) :: tail) =>
-                val nodeIds = tail.map {
-                  case JString(nodeId) => nodeId
-                }
-                Boot.system.actorSelection(Register.actorPathToNodeId(nodeIds.head))
-                  .resolveOne(2 seconds)
+      respondWithDefaultHeaders(customHeaders) {
+        post {
+          entity(as[String]) {
+            body =>
+              val json = parse(body).extract[JsonRPCBody]
+              val f_res: Future[AnyRef] = json match {
+                case JsonRPCBody(_, _, "connect", JString(host) :: JInt(port) :: JInt(anchor_amount) :: Nil) =>
+                  connect(new InetSocketAddress(host, port.toInt), anchor_amount.toLong)
+                  Future.successful("")
+                case JsonRPCBody(_, _, "list", _) =>
+                  (register ? ListChannels).mapTo[Iterable[ActorRef]]
+                    .flatMap(l => Future.sequence(l.map(c => c ? CMD_GETINFO)))
+                case JsonRPCBody(_, _, "addhtlc", JInt(amount) :: JString(rhash) :: JInt(expiry) :: tail) =>
+                  val nodeIds = tail.map {
+                    case JString(nodeId) => nodeId
+                  }
+                  Boot.system.actorSelection(Register.actorPathToNodeId(nodeIds.head))
+                    .resolveOne(2 seconds)
                     .map { channel =>
                       channel ! CMD_ADD_HTLC(amount.toInt, BinaryData(rhash), locktime(Seconds(expiry.toInt)), nodeIds.drop(1))
                       channel.toString()
                     }
-              case JsonRPCBody(_, _, "sign", JString(channel) :: Nil) =>
-                sendCommand(channel, CMD_SIGN)
-              case JsonRPCBody(_, _, "fulfillhtlc", JString(channel) :: JInt(id) :: JString(r) :: Nil) =>
-                sendCommand(channel, CMD_FULFILL_HTLC(id.toLong, BinaryData(r)))
-              case JsonRPCBody(_, _, "close", JString(channel) :: JString(scriptPubKey) :: Nil) =>
-                sendCommand(channel, CMD_CLOSE(Some(scriptPubKey)))
-              case JsonRPCBody(_, _, "close", JString(channel) :: Nil) =>
-                sendCommand(channel, CMD_CLOSE(None))
-              case JsonRPCBody(_, _, "help", _) =>
-                Future.successful(List(
-                  "connect (host, port, anchor_amount): opens a channel with another eclair or lightningd instance",
-                  "list: lists existing channels",
-                  "addhtlc (channel_id, amount, rhash, locktime): sends an htlc",
-                  "sign (channel_id): updates the commitment transaction",
-                  "fulfillhtlc (channel_id, r): fulfills an htlc",
-                  "close (channel_id): closes a channel",
-                  "help: displays this message"))
-              case _ => Future.failed(new RuntimeException("method not found"))
-            }
+                case JsonRPCBody(_, _, "sign", JString(channel) :: Nil) =>
+                  sendCommand(channel, CMD_SIGN)
+                case JsonRPCBody(_, _, "fulfillhtlc", JString(channel) :: JInt(id) :: JString(r) :: Nil) =>
+                  sendCommand(channel, CMD_FULFILL_HTLC(id.toLong, BinaryData(r)))
+                case JsonRPCBody(_, _, "close", JString(channel) :: JString(scriptPubKey) :: Nil) =>
+                  sendCommand(channel, CMD_CLOSE(Some(scriptPubKey)))
+                case JsonRPCBody(_, _, "close", JString(channel) :: Nil) =>
+                  sendCommand(channel, CMD_CLOSE(None))
+                case JsonRPCBody(_, _, "help", _) =>
+                  Future.successful(List(
+                    "connect (host, port, anchor_amount): opens a channel with another eclair or lightningd instance",
+                    "list: lists existing channels",
+                    "addhtlc (channel_id, amount, rhash, locktime): sends an htlc",
+                    "sign (channel_id): updates the commitment transaction",
+                    "fulfillhtlc (channel_id, r): fulfills an htlc",
+                    "close (channel_id): closes a channel",
+                    "help: displays this message"))
+                case _ => Future.failed(new RuntimeException("method not found"))
+              }
 
-            onComplete(f_res) {
-              case Success(res) => complete(HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, Serialization.writePretty(
-                JsonRPCRes(res, None, json.id)
-              ))))
-              case Failure(t) => complete(HttpResponse(StatusCodes.InternalServerError, entity = HttpEntity(ContentTypes.`application/json`, Serialization.writePretty(
-                JsonRPCRes(null, Some(Error(-1, t.getMessage)), json.id))
-              )))
-            }
+              onComplete(f_res) {
+                case Success(res) => complete(HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, Serialization.writePretty(
+                  JsonRPCRes(res, None, json.id)
+                ))))
+                case Failure(t) => complete(HttpResponse(StatusCodes.InternalServerError, entity = HttpEntity(ContentTypes.`application/json`, Serialization.writePretty(
+                  JsonRPCRes(null, Some(Error(-1, t.getMessage)), json.id))
+                )))
+              }
+          }
         }
       }
     }
