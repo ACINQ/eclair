039081664a504c163532d17983f7785cb4ea575c
pm47 <spam4420@gmail.com>
2016-08-24 19:39:34 +0200

    removed text in icon

--- eclair-demo/src/main/scala/fr/acinq/eclair/gui/DialogSplash.scala
+++ eclair-demo/src/main/scala/fr/acinq/eclair/gui/DialogSplash.scala
@@ -18,7 +18,8 @@ class DialogSplash(primaryStage: Stage) extends Stage() {
   setResizable(false)
 
   val image = new Image("/eclair01.png", true)
-  primaryStage.getIcons().add(image)
+  val icon = new Image("/eclair02.png", true)
+  primaryStage.getIcons().add(icon)
   val view = new ImageView(image)
 
   val pane = new StackPane()
