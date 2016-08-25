32f0784abdf96cdbaf2a2301b3c22970cd96f893
sstone <sstone@users.noreply.github.com>
2016-08-25 18:14:21 +0200

    update interop test

--- eclair-demo/src/test/scala/fr/acinq/eclair/interop/InteroperabilitySpec.scala
+++ eclair-demo/src/test/scala/fr/acinq/eclair/interop/InteroperabilitySpec.scala
@@ -32,9 +32,7 @@ object LinuxOnlyTest extends Tag("fr.acinq.eclair.tags.LinuxOnlyTest")
 /**
   * this test is ignored by default
   * to run it:
-  * mvn exec:java -Dexec.mainClass="org.scalatest.tools.Runner" -Dexec.classpathScope="test"\
-  * -Dexec.args="-o -s fr.acinq.eclair.interop.InteroperabilitySpec"
-  *
+  * mvn exec:java -Dexec.mainClass="org.scalatest.tools.Runner" -Dexec.classpathScope="test" -Dexec.args="-o -s fr.acinq.eclair.interop.InteroperabilitySpec"
   */
 class InteroperabilitySpec extends TestKit(ActorSystem("test")) with FunSuiteLike with BeforeAndAfterAll {
 
@@ -93,7 +91,7 @@ class InteroperabilitySpec extends TestKit(ActorSystem("test")) with FunSuiteLik
     "PATH" -> s"$currentdir/$prefix").run
   val lightningdf = Future(blocking(lightningd.exitValue()))
   sys.addShutdownHook(lightningd.destroy())
-  Thread.sleep(500)
+  Thread.sleep(5000) // lightning now takes more time to start b/c of sqlite
   assert(!lightningdf.isCompleted)
 
   val chain = Await.result(bitcoinClient.invoke("getblockchaininfo").map(json => (json \ "chain").extract[String]), 10 seconds)
@@ -182,6 +180,7 @@ class InteroperabilitySpec extends TestKit(ActorSystem("test")) with FunSuiteLik
       htlcid <- listChannels.map(_.head).map(_.data.asInstanceOf[DATA_NORMAL].commitments.theirCommit.spec.htlcs.head.add.id)
       _ <- sendCommand(channelId, CMD_FULFILL_HTLC(htlcid, Helpers.revocationPreimage(seed, 0)))
       _ <- sendCommand(channelId, CMD_SIGN)
+      _ = Thread.sleep(500)
       peer1 = lncli.getPeers.head
       _ = assert(peer1.their_amount + peer1.their_fee == 70000000)
       // lightningd sends us another htlc
@@ -192,20 +191,21 @@ class InteroperabilitySpec extends TestKit(ActorSystem("test")) with FunSuiteLik
       htlcid1 <- listChannels.map(_.head).map(_.data.asInstanceOf[DATA_NORMAL].commitments.theirCommit.spec.htlcs.head.add.id)
       _ <- sendCommand(channelId, CMD_FULFILL_HTLC(htlcid1, Helpers.revocationPreimage(seed, 1)))
       _ <- sendCommand(channelId, CMD_SIGN)
+      _ = Thread.sleep(500)
       peer2 = lncli.getPeers.head
       _ = assert(peer2.their_amount + peer2.their_fee == 70000000 + 80000000)
       // we send lightningd a HTLC
-      _ <- sendCommand(channelId, CMD_ADD_HTLC(70000000, Helpers.revocationHash(seed, 0), locktime(Blocks(blockcount.toInt + 576))))
+      _ <- sendCommand(channelId, CMD_ADD_HTLC(70000000, Helpers.revocationHash(seed, 0), locktime(Blocks(blockcount.toInt + 576)), id = Some(42)))
       _ <- sendCommand(channelId, CMD_SIGN)
       _ = Thread.sleep(500)
       // and we ask lightingd to fulfill it
-      _ = lncli.fulfillhtlc(peer.peerid, Helpers.revocationPreimage(seed, 0))
+      _ = lncli.fulfillhtlc(peer.peerid, 42, Helpers.revocationPreimage(seed, 0))
       _ = Thread.sleep(500)
       _ <- sendCommand(channelId, CMD_SIGN)
       c <- listChannels.map(_.head).map(_.data.asInstanceOf[DATA_NORMAL].commitments)
       _ = assert(c.ourCommit.spec.amount_us_msat == 80000000)
     } yield ()
-    Await.result(future, 30 seconds)
+    Await.result(future, 300000 seconds)
   }
 
   test("close the channel") {
@@ -274,8 +274,8 @@ object InteroperabilitySpec {
       assert(s"$path newhtlc $peerid $amount $expiry $rhash".! == 0)
     }
 
-    def fulfillhtlc(peerid: String, rhash: BinaryData): Unit = {
-      assert(s"$path fulfillhtlc $peerid $rhash".! == 0)
+    def fulfillhtlc(peerid: String, htlcId: Long, rhash: BinaryData): Unit = {
+      assert(s"$path fulfillhtlc $peerid $htlcId $rhash".! == 0)
     }
 
     def commit(peerid: String): Unit = {
