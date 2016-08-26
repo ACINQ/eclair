9cd54615a851f6e80152c1ba54ce2af11290ceb1
pm47 <spam4420@gmail.com>
2016-08-26 13:15:11 +0200

    minor improvements to payment request dialog

--- eclair-demo/src/main/scala/fr/acinq/eclair/gui/DialogReceive.scala
+++ eclair-demo/src/main/scala/fr/acinq/eclair/gui/DialogReceive.scala
@@ -2,10 +2,12 @@ package fr.acinq.eclair.gui
 
 import javafx.event.{ActionEvent, EventHandler}
 import javafx.geometry.{Insets, Pos}
-import javafx.scene.{Node, Scene}
+import javafx.scene.Scene
 import javafx.scene.control.{Button, Label, TextArea, TextField}
 import javafx.scene.layout.GridPane
 import javafx.stage.{Modality, Stage, StageStyle}
+
+import scala.util.{Success, Try}
 
 /**
   * Created by PM on 16/08/2016.
@@ -34,33 +36,28 @@ class DialogReceive(primaryStage: Stage, handlers: Handlers) extends Stage() {
   val textFieldAmountMsat = new TextField()
   grid.add(textFieldAmountMsat, 1, 0)
 
+  val btn = new Button("Generate")
+  btn.defaultButtonProperty().bind(btn.focusedProperty())
+  grid.add(btn, 1, 1)
+
   val labelPaymentRequest = new Label("Payment Request")
-  grid.add(labelPaymentRequest, 0, 1)
+  grid.add(labelPaymentRequest, 0, 2)
 
   val textAreaPaymentRequest = new TextArea()
   textAreaPaymentRequest.setWrapText(true)
   textAreaPaymentRequest.setPrefRowCount(2)
   textAreaPaymentRequest.setPrefColumnCount(40)
   textAreaPaymentRequest.setEditable(false)
-  grid.add(textAreaPaymentRequest, 1, 1)
+  grid.add(textAreaPaymentRequest, 1, 2)
 
-  val btn = new Button("Generate")
   btn.setOnAction(new EventHandler[ActionEvent] {
     override def handle(event: ActionEvent): Unit = {
-      handlers.getPaymentRequest(textFieldAmountMsat.getText.toLong, textAreaPaymentRequest)
+      Try(textFieldAmountMsat.getText.toLong) match {
+        case Success(amountMsat) => handlers.getPaymentRequest(textFieldAmountMsat.getText.toLong, textAreaPaymentRequest)
+        case _ => {}
+      }
     }
   })
-  grid.add(btn, 1, 2)
-  //  val labelH = new Label("H")
-  //  grid.add(labelH, 0, 0)
-  //
-  //  val textFieldH = new TextField()
-  //  textFieldH.setEditable(false)
-  //  grid.add(textFieldH, 1, 0)
-  //
-  //
-  //
-  //  handlers.getH(textFieldH)
 
   val scene = new Scene(grid)
   setScene(scene)
