015e56456f81251be426c3b5fddaa6e3346b35dc
pm47 <pm.padiou@gmail.com>
2017-02-02 16:26:16 +0100

    improved transport-level serializer

--- eclair-node/src/main/scala/fr/acinq/eclair/io/LightningMessageSerializer.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/io/LightningMessageSerializer.scala
@@ -4,17 +4,22 @@ import fr.acinq.bitcoin.BinaryData
 import fr.acinq.eclair.crypto.TransportHandler
 import fr.acinq.eclair.wire.{Codecs, LightningMessage}
 import scodec.bits.BitVector
+import scodec.{Attempt, DecodeResult}
 
 /**
   * Created by fabrice on 16/01/17.
   */
 object LightningMessageSerializer extends TransportHandler.Serializer[LightningMessage] {
-  override def serialize(t: LightningMessage): BinaryData = {
-    val encoded = Codecs.lightningMessageCodec.encode(t)
-    encoded.toOption.map(_.toByteArray).getOrElse(throw new RuntimeException(s"cannot encode $t"))
-  }
 
-  override def deserialize(bin: BinaryData): LightningMessage = {
-    Codecs.lightningMessageCodec.decode(BitVector(bin.toArray)).toOption.get.value
-  }
+  override def serialize(t: LightningMessage): BinaryData =
+    Codecs.lightningMessageCodec.encode(t) match {
+      case Attempt.Successful(bitVector) => BinaryData(bitVector.toByteArray)
+      case Attempt.Failure(cause) => throw new RuntimeException(s"serialization error: $cause")
+    }
+
+  override def deserialize(bin: BinaryData): LightningMessage =
+    Codecs.lightningMessageCodec.decode(BitVector(bin.data)) match {
+      case Attempt.Successful(DecodeResult(msg, _)) => msg
+      case Attempt.Failure(cause) => throw new RuntimeException(s"deserialization error: $cause")
+    }
 }
