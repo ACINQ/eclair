f75706df9d8f45608e25167190c1d27ca0234288
sstone <sstone@users.noreply.github.com>
2017-02-09 11:19:19 +0100

    PeerHandler: fix tcp framing issue

--- eclair-node/src/main/scala/fr/acinq/eclair/blockchain/peer/PeerHandler.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/blockchain/peer/PeerHandler.scala
@@ -1,6 +1,7 @@
 package fr.acinq.eclair.blockchain.peer
 
 import java.net.InetSocketAddress
+import java.nio.ByteOrder
 
 import akka.actor._
 import akka.io.{IO, Tcp}
@@ -17,6 +18,7 @@ import scala.util.{Failure, Success, Try}
   */
 class PeerHandler(remote: InetSocketAddress, listener: ActorRef) extends Actor with ActorLogging {
 
+  import PeerHandler._
   import akka.io.Tcp._
 
   implicit val system = context.system
@@ -46,28 +48,70 @@ class PeerHandler(remote: InetSocketAddress, listener: ActorRef) extends Actor w
       val connection = sender()
       listener ! c
       connection ! Register(self)
-      context become connected(connection)
+      context become connected(connection, Nil)
   }
 
-  def connected(connection: ActorRef): Receive = {
+  def connected(connection: ActorRef, buffer: Seq[Byte]): Receive = {
     case message: fr.acinq.bitcoin.Message =>
       log.debug(s"sending $message")
       connection ! Write(ByteString(fr.acinq.bitcoin.Message.write(message).toArray))
+
     case CommandFailed(w: Write) =>
       // O/S buffer was full
       log.error("write failed")
+
     case Received(data) =>
       log.debug(s"received $data")
-      Try(fr.acinq.bitcoin.Message.read(data.toArray)) match {
-        case Success(message) => listener ! message
-        case Failure(cause) => log.error(cause, s"cannot parse ${toHexString(data.toArray)}")
-      }
+      // peer sent some data
+      // extract bitcoin messages and process them
+      val (messages, remainder) = extract(buffer ++ data)
+      messages.map(message => listener ! message)
+      context become connected(connection, remainder)
+      
     case 'close =>
       connection ! Close
+
     case _: ConnectionClosed =>
       log.info(s"connection to $remote closed")
       context stop self
   }
+}
+
+object PeerHandler {
+  /**
+    * extract bitcoin messages from raw data
+    *
+    * @param data raw data
+    * @param acc  messages that we have so far
+    * @return a (messages, remainder) tuple where messages are all the messages that could be extracted and
+    *         remainder is what is left of the input data (i.e could not be parsed because the message it contains
+    *         has not been fully received yet
+    */
+  def extract(data: Seq[Byte], acc: Vector[Message] = Vector.empty[Message]): (Vector[Message], Seq[Byte]) = {
+    if (data.length < 24) {
+      (acc, data)
+    } else {
+      val magic = Protocol.uint32(data, ByteOrder.LITTLE_ENDIAN)
+      val data1 = data.drop(4)
+      val command = extractCommand(data1)
+      val data2 = data1.drop(12)
+      val length = Protocol.uint32(data2, ByteOrder.LITTLE_ENDIAN)
+      val data3 = data2.drop(4)
+      val checksum = Protocol.uint32(data3, ByteOrder.LITTLE_ENDIAN)
+      val data4 = data3.drop(4)
+      if (data4.length >= length) {
+        val payload = data4.take(length.toInt)
+        require(checksum == computeChecksum(payload), "invalid checksum")
+        extract(data4.drop(length.toInt), acc :+ Message(magic, command, BinaryData(payload)))
+      } else (acc, data)
+    }
+  }
+
+  def extractCommand(data: Seq[Byte]): String = new String(data.take(12).takeWhile(_ != 0).toArray, "ISO-8859-1")
+
+  def computeChecksum(payload: Seq[Byte]): Long = Protocol.uint32(Crypto.hash256(payload).take(4), ByteOrder.LITTLE_ENDIAN)
+
+  def verifyChecksum(checksum: Long, payload: Seq[Byte]): Boolean = checksum == computeChecksum(payload)
 }
 
 
