3b271cb6c2fe4d5bcac5d3acc8ff52ab5d1027d8
sstone <sstone@users.noreply.github.com>
2017-02-09 11:43:51 +0100

    make PeerClient ask for witness tx

--- eclair-node/src/main/scala/fr/acinq/eclair/blockchain/peer/PeerClient.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/blockchain/peer/PeerClient.scala
@@ -1,6 +1,6 @@
 package fr.acinq.eclair.blockchain.peer
 
-import java.net.InetSocketAddress
+import java.net.{InetAddress, InetSocketAddress}
 
 import akka.actor._
 import akka.io.Tcp.Connected
@@ -33,8 +33,8 @@ class PeerClient extends Actor with ActorLogging {
   override def receive: Actor.Receive = {
     case Connected(remote, local) =>
       val version = Version(
-        70002L,
-        services = 1L,
+        70015L,
+        services = 1L | (1 << 3),
         timestamp = Platform.currentTime / 1000,
         addr_recv = NetworkAddress(1L, local.getAddress, local.getPort.toLong),
         addr_from = NetworkAddress(1L, remote.getAddress, remote.getPort.toLong),
@@ -47,21 +47,34 @@ class PeerClient extends Actor with ActorLogging {
       val version = Version.read(payload)
       log.debug(s"received $version")
       sender ! Message(magic, "verack", Array.empty[Byte])
+      sender ! Message(magic, "mempool", Array.empty[Byte])
+      val inventory = Inventory(InventoryVector(1, BinaryData("3545255e0755f7b39115f1da928fd388291c135dbcbf0f45a0563a02a2449d32").reverse) :: InventoryVector(1, BinaryData("124ba11b7ca3299b9f74f83baafd6db154cddc313e99aa50bbbc01c7f91eb411").reverse) :: InventoryVector(1, ("ee75fd2a7bbdf0bbbe363ebb6e2285ee0d5e2240c0cba088139eb49545203646").reverse) :: Nil)
+      import scala.concurrent.ExecutionContext.Implicits.global
+      context.system.scheduler.scheduleOnce(15 seconds, sender, Message(magic, "getdata", Inventory.write(inventory)))
     case Message(magic, "verack", _) =>
       log.debug("received verack")
     case Message(magic, "inv", payload) =>
       val inventory = Inventory.read(payload)
       log.debug(s"received $inventory")
-      sender ! Message(magic, "getdata", payload)
+      // see https://github.com/bitcoin/bips/blob/master/bip-0144.mediawiki: request tx with witness
+      val inventory1 = Inventory(inventory.inventory.map(iv => iv.`type` match {
+        case 1 => iv.copy(`type` = 0x40000001)
+        case _ => iv
+      }))
+      sender ! Message(magic, "getdata", Inventory.write(inventory1))
     case Message(magic, "ping", payload) =>
       log.debug(s"received a ping")
       sender ! Message(magic, "pong", payload)
     case Message(magic, "tx", payload) =>
-      log.debug(s"received tx ${toHexString(payload)}")
-      context.system.eventStream.publish(NewTransaction(Transaction.read(payload)))
+      val tx = Transaction.read(payload)
+      log.debug(s"received tx ${tx.txid}")
+      context.system.eventStream.publish(NewTransaction(tx))
     case Message(magic, "block", payload) =>
       log.debug(s"received block ${toHexString(payload)}")
       context.system.eventStream.publish(NewBlock(Block.read(payload)))
+    case Message(magic, "notfound", payload) =>
+      val inventory = Inventory.read(payload)
+      log.debug(s"received notfound for inv $inventory")
     case Message(magic, command, payload) =>
       log.debug(s"received unknown $command ${toHexString(payload)}")
   }
@@ -77,5 +90,27 @@ object PeerClientTest extends App {
     }
   }), name = "listener")
   system.eventStream.subscribe(listener, classOf[BlockchainEvent])
+
 }
 
+object Test extends App {
+
+  val local = InetAddress.getLocalHost
+  val remote = InetAddress.getLocalHost
+  val version = Version(
+    70015L,
+    services = 1L,
+    timestamp = Platform.currentTime / 1000,
+    addr_recv = NetworkAddress(1L, local, 42000),
+    addr_from = NetworkAddress(1L, remote, 42001),
+    nonce = 0x4317be39ae6ea291L,
+    user_agent = "eclair:alpha",
+    start_height = 0x0L,
+    relay = true)
+  val msg = Message(Message.MagicTestnet3, command = "version", payload = Version.write(version))
+  val bin = Message.write(msg) ++ Message.write(msg)
+
+  val msg1 = Message.read(bin)
+  println(msg1)
+
+}
