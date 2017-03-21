ca2b678fd4c5a70725211d794121d20e17d3055d
dpad85 <dpad85@users.noreply.github.com>
2017-03-21 11:18:00 +0100

    (gui) updated about

--- eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/AboutController.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/AboutController.scala
@@ -13,4 +13,5 @@ class AboutController(hostServices: HostServices) extends Logging {
   @FXML def openApacheLicencePage = hostServices.showDocument("https://www.apache.org/licenses/LICENSE-2.0")
   @FXML def openACINQPage = hostServices.showDocument("https://acinq.co")
   @FXML def openGithubPage = hostServices.showDocument("https://github.com/ACINQ/eclair")
+  @FXML def openLNRFCPage = hostServices.showDocument("https://github.com/lightningnetwork/lightning-rfc")
 }
