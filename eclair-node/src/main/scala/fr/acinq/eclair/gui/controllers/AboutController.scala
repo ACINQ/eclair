322da61503966d73c786d18025f6c56bd8a97378
dpad85 <dpad85@users.noreply.github.com>
2017-03-23 19:41:06 +0100

    (gui) version in about is now dynamic; ESC key closes about

--- eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/AboutController.scala
+++ eclair-node/src/main/scala/fr/acinq/eclair/gui/controllers/AboutController.scala
@@ -1,8 +1,10 @@
 package fr.acinq.eclair.gui.controllers
 
 import javafx.fxml.FXML
-import javafx.application.HostServices;
+import javafx.application.HostServices
+import javafx.scene.text.Text
 
+import fr.acinq.eclair.Version
 import grizzled.slf4j.Logging
 
 /**
@@ -10,6 +12,10 @@ import grizzled.slf4j.Logging
   */
 class AboutController(hostServices: HostServices) extends Logging {
 
+  @FXML var version: Text = _
+  @FXML def initialize = {
+    version.setText(Version.version)
+  }
   @FXML def openApacheLicencePage = hostServices.showDocument("https://www.apache.org/licenses/LICENSE-2.0")
   @FXML def openACINQPage = hostServices.showDocument("https://acinq.co")
   @FXML def openGithubPage = hostServices.showDocument("https://github.com/ACINQ/eclair")
