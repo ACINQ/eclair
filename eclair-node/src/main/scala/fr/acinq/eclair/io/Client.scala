ACINQ/eclair

commit 75e4923002bdca8b04c2703cf6682a4b5da3ddf8

BOLT 4: use proper cipher stream for reply messages (#75)
* more logs on connection establishment, added initialize() to FSMs

eclair-node/src/main/scala/fr/acinq/eclair/io/Client.scala
@@ -38,17 +38,24 @@ class Client(nodeParams: NodeParams, switchboard: ActorRef, address: InetSocketA
           serializer = LightningMessageSerializer)))
       connection ! akka.io.Tcp.Register(transport)
       context watch transport
-      context become connected(transport)
+      context become authenticating(transport)
   }
 
-  def connected(transport: ActorRef): Receive = {
+  def authenticating(transport: ActorRef): Receive = {
     case Terminated(actor) if actor == transport =>
       origin ! Status.Failure(new RuntimeException("authentication failed"))
       context stop self
 
     case h: HandshakeCompleted =>
+      log.info(s"handshake completed with ${h.remoteNodeId}")
       origin ! "connected"
       switchboard ! h
+      context unwatch transport
+      context become connected(transport)
+  }
+
+  def connected(transport: ActorRef): Receive = {
+    case msg => log.warning(s"unexpected message $msg")
   }
 
   // we should not restart a failing transport

eclair-node/src/main/scala/fr/acinq/eclair/io/Peer.scala
@@ -47,8 +47,7 @@ class Peer(nodeParams: NodeParams, remoteNodeId: PublicKey, address_opt: Option[
   import Peer._
   import scala.concurrent.ExecutionContext.Implicits.global
 
-  startWith(DISCONNECTED, DisconnectedData(Nil))
-  if (nodeParams.autoReconnect) self ! Reconnect
+  startWith(DISCONNECTED, DisconnectedData(Nil), if (nodeParams.autoReconnect) Some(3 seconds) else None)
 
   when(DISCONNECTED, stateTimeout = if (nodeParams.autoReconnect) 60 seconds else null) {
     case Event(state: HasCommitments, d@DisconnectedData(offlineChannels)) =>
@@ -228,6 +227,8 @@ class Peer(nodeParams: NodeParams, remoteNodeId: PublicKey, address_opt: Option[
 
   // a failing channel won't be restarted, it should handle its states
   override val supervisorStrategy = OneForOneStrategy(loggingEnabled = true) { case _ => SupervisorStrategy.Stop }
+
+  initialize()
 
 }
 

eclair-node/src/main/scala/fr/acinq/eclair/channel/Channel.scala
@@ -994,6 +994,7 @@ class Channel(val nodeParams: NodeParams, remoteNodeId: PublicKey, blockchain: A
   }
 
   onTransition {
+    case WAIT_FOR_INIT_INTERNAL -> WAIT_FOR_INIT_INTERNAL => {} // called at channel initialization
     case WAIT_FOR_INIT_INTERNAL -> OFFLINE =>
       context.system.eventStream.publish(ChannelStateChanged(self, context.parent, remoteNodeId, WAIT_FOR_INIT_INTERNAL, OFFLINE, nextStateData))
     case state -> nextState =>
@@ -1212,6 +1213,8 @@ class Channel(val nodeParams: NodeParams, remoteNodeId: PublicKey, blockchain: A
 
   // we let the peer decide what to do
   override val supervisorStrategy = OneForOneStrategy(loggingEnabled = true) { case _ => SupervisorStrategy.Escalate }
+
+  initialize()
 
 }
 

eclair-node/src/main/scala/fr/acinq/eclair/crypto/Sphinx.scala
@@ -327,8 +327,9 @@ object Sphinx {
     */
   def forwardErrorPacket(packet: BinaryData, sharedSecret: BinaryData): BinaryData = {
     require(packet.length == ErrorPacketLength, s"invalid error packet length ${packet.length}, must be $ErrorPacketLength")
-    val filler = Sphinx.generateFiller("ammag", Seq(sharedSecret), ErrorPacketLength, 1)
-    Sphinx.xor(packet, filler)
+    val key = generateKey("ammag", sharedSecret)
+    val stream = generateStream(key, ErrorPacketLength)
+    Sphinx.xor(packet, stream)
   }
 
   /**

eclair-node/src/main/scala/fr/acinq/eclair/crypto/TransportHandler.scala
@@ -59,6 +59,7 @@ class TransportHandler[T: ClassTag](keyPair: KeyPair, rs: Option[BinaryData], co
 
   when(Handshake) {
     case Event(Received(data), HandshakeData(reader, buffer)) =>
+      log.debug(s"received ${BinaryData(data)}")
       val buffer1 = buffer ++ data
       if (buffer1.length < expectedLength(reader))
         stay using HandshakeData(reader, buffer1)
@@ -132,13 +133,15 @@ class TransportHandler[T: ClassTag](keyPair: KeyPair, rs: Option[BinaryData], co
       stop(FSM.Normal)
 
     case Event(Terminated(actor), _) if actor == connection =>
+      log.warning(s"connection terminated, stopping the transport")
       // this can be the connection or the listener, either way it is a cause of death
       stop(FSM.Normal)
-
-    case Event(message, _) =>
-      log.warning(s"unhandled $message")
-      stay()
   }
+
+  override def aroundPostStop(): Unit = connection ! Close
+
+  initialize()
+
 }
 
 object TransportHandler {

eclair-node/src/main/scala/fr/acinq/eclair/io/Server.scala
@@ -2,7 +2,7 @@ package fr.acinq.eclair.io
 
 import java.net.InetSocketAddress
 
-import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, SupervisorStrategy}
+import akka.actor.{Actor, ActorLogging, ActorRef, OneForOneStrategy, Props, Status, SupervisorStrategy, Terminated}
 import akka.io.Tcp.SO.KeepAlive
 import akka.io.{IO, Tcp}
 import fr.acinq.eclair.NodeParams
@@ -23,9 +23,7 @@ class Server(nodeParams: NodeParams, switchboard: ActorRef, address: InetSocketA
 
   IO(Tcp) ! Bind(self, address, options = KeepAlive(true) :: Nil)
 
-  def receive() = main(Set())
-
-  def main(transports: Set[ActorRef]): Receive = {
+  def receive() = {
     case Bound(localAddress) =>
       bound.map(_.success())
       log.info(s"bound on $localAddress")
@@ -44,9 +42,9 @@ class Server(nodeParams: NodeParams, switchboard: ActorRef, address: InetSocketA
           connection = connection,
           serializer = LightningMessageSerializer)))
       connection ! akka.io.Tcp.Register(transport)
-      context become main(transports + transport)
 
     case h: HandshakeCompleted =>
+      log.info(s"handshake completed with ${h.remoteNodeId}")
       switchboard ! h
   }
 

eclair-node/src/main/scala/fr/acinq/eclair/io/Switchboard.scala
@@ -42,7 +42,7 @@ class Switchboard(nodeParams: NodeParams, watcher: ActorRef, router: ActorRef, r
           sender ! s"already connected to nodeId=$remoteNodeId"
           connection
         case None =>
-          log.info(s"connecting to $remoteNodeId @ $address")
+          log.info(s"connecting to $remoteNodeId @ $address on behalf of $sender")
           val connection = context.actorOf(Client.props(nodeParams, self, address, remoteNodeId, sender))
           context watch (connection)
           connection

eclair-node/src/main/scala/fr/acinq/eclair/payment/PaymentLifecycle.scala
@@ -114,6 +114,8 @@ class PaymentLifecycle(sourceNodeId: PublicKey, router: ActorRef, register: Acto
       }
 
   }
+
+  initialize()
 }
 
 object PaymentLifecycle {

commit aa22f66af83ac02136984792978ecb592c75e500

client must die when transport dies (regression caused in 75e4923002bdca8b04c2703cf6682a4b5da3ddf8)

eclair-node/src/main/scala/fr/acinq/eclair/io/Client.scala
@@ -50,11 +50,13 @@ class Client(nodeParams: NodeParams, switchboard: ActorRef, address: InetSocketA
       log.info(s"handshake completed with ${h.remoteNodeId}")
       origin ! "connected"
       switchboard ! h
-      context unwatch transport
       context become connected(transport)
   }
 
   def connected(transport: ActorRef): Receive = {
+    case Terminated(actor) if actor == transport =>
+      context stop self
+
     case msg => log.warning(s"unexpected message $msg")
   }
 

eclair-node/src/main/scala/fr/acinq/eclair/io/Peer.scala
@@ -75,10 +75,15 @@ class Peer(nodeParams: NodeParams, remoteNodeId: PublicKey, address_opt: Option[
 
     case Event(Rebroadcast(_), _) => stay // ignored
 
+    case Event("connected", _) => stay // ignored
+
+    case Event(StateTimeout, d: DisconnectedData) if d.offlineChannels.size == 0 => stay // ignored
+
     case Event(StateTimeout, _) =>
       log.info(s"attempting a reconnect")
       self ! Reconnect
       stay
+
   }
 
   when(INITIALIZING) {