fad455055d8bfb388cb6cd4d2af431d876363895
pm47 <spam4420@gmail.com>
2017-06-15 15:35:55 +0200

    put the arguments of the 'open' http method in the same order as the uri

--- eclair-core/src/main/scala/fr/acinq/eclair/api/Service.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/api/Service.scala
@@ -81,7 +81,7 @@ trait Service extends Logging {
               val f_res: Future[AnyRef] = req match {
                 case JsonRPCBody(_, _, "connect", JString(host) :: JInt(port) :: JString(nodeId) :: Nil) =>
                   (switchboard ? NewConnection(PublicKey(nodeId), new InetSocketAddress(host, port.toInt), None)).mapTo[String]
-                case JsonRPCBody(_, _, "open", JString(host) :: JInt(port) :: JString(nodeId) :: JInt(fundingSatoshi) :: JInt(pushMsat) :: Nil) =>
+                case JsonRPCBody(_, _, "open", JString(nodeId) :: JString(host) :: JInt(port) :: JInt(fundingSatoshi) :: JInt(pushMsat) :: Nil) =>
                   (switchboard ? NewConnection(PublicKey(nodeId), new InetSocketAddress(host, port.toInt), Some(NewChannel(Satoshi(fundingSatoshi.toLong), MilliSatoshi(pushMsat.toLong))))).mapTo[String]
                 case JsonRPCBody(_, _, "peers", _) =>
                   (switchboard ? 'peers).mapTo[Map[PublicKey, ActorRef]].map(_.map(_._1.toBin))
@@ -102,7 +102,7 @@ trait Service extends Logging {
                 case JsonRPCBody(_, _, "help", _) =>
                   Future.successful(List(
                     "connect (host, port, nodeId): connect to another lightning node through a secure connection",
-                    "open (host, port, nodeId, fundingSatoshi, pushMsat): open a channel with another lightning node",
+                    "open (nodeId, host, port, fundingSatoshi, pushMsat): open a channel with another lightning node",
                     "peers: list existing local peers",
                     "channels: list existing local channels",
                     "channel (channelId): retrieve detailed information about a given channel",
