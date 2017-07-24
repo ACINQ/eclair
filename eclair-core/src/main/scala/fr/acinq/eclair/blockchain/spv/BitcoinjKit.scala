04d08cfc01328948459c2a63d2ce3405975cf3ae
pm47 <pm.padiou@gmail.com>
2017-07-24 16:19:57 +0200

    adds a way to tell when we know the current height
    This is because we need to use correct block height when sending
    htcs, otherwise counterparties will unilaterally close the channel

--- eclair-core/src/main/scala/fr/acinq/eclair/blockchain/spv/BitcoinjKit.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/blockchain/spv/BitcoinjKit.scala
@@ -1,22 +1,19 @@
 package fr.acinq.eclair.blockchain.spv
 
 import java.io.File
-import java.util
 
 import akka.actor.ActorSystem
-import fr.acinq.bitcoin.{BinaryData, Transaction}
+import fr.acinq.bitcoin.Transaction
 import fr.acinq.eclair.Globals
 import fr.acinq.eclair.blockchain.spv.BitcoinjKit._
 import fr.acinq.eclair.blockchain.{CurrentBlockCount, NewConfidenceLevel}
 import grizzled.slf4j.Logging
 import org.bitcoinj.core.TransactionConfidence.ConfidenceType
-import org.bitcoinj.core.listeners.{NewBestBlockListener, OnTransactionBroadcastListener, PeerConnectedEventListener, TransactionConfidenceEventListener}
+import org.bitcoinj.core.listeners._
 import org.bitcoinj.core.{NetworkParameters, Peer, StoredBlock, Transaction => BitcoinjTransaction}
 import org.bitcoinj.kits.WalletAppKit
 import org.bitcoinj.params.{RegTestParams, TestNet3Params}
-import org.bitcoinj.script.Script
 import org.bitcoinj.wallet.Wallet
-import org.bitcoinj.wallet.listeners.ScriptsChangeEventListener
 
 import scala.concurrent.Promise
 
@@ -25,43 +22,57 @@ import scala.concurrent.Promise
   */
 class BitcoinjKit(chain: String, datadir: File)(implicit system: ActorSystem) extends WalletAppKit(chain2Params(chain), datadir, "bitcoinj") with Logging {
 
-  // so that we know when the peerGroup/chain/wallet are accessible
+  // tells us when the peerGroup/chain/wallet are accessible
   private val initializedPromise = Promise[Boolean]()
   val initialized = initializedPromise.future
 
+  // tells us as soon as we know the current block height
+  private val atCurrentHeightPromise = Promise[Boolean]()
+  val atCurrentHeight = atCurrentHeightPromise.future
+
+  // tells us when we are at current block height
+//  private val syncedPromise = Promise[Boolean]()
+//  val synced = syncedPromise.future
+
+  private def updateBlockCount(blockCount: Int) = {
+    // when synchronizing we don't want to advertise previous blocks
+    if (Globals.blockCount.get() < blockCount) {
+      logger.debug(s"current blockchain height=$blockCount")
+      system.eventStream.publish(CurrentBlockCount(blockCount))
+      Globals.blockCount.set(blockCount)
+    }
+  }
+
   override def onSetupCompleted(): Unit = {
 
-    logger.info(s"peerGroup.getMaxConnections==${peerGroup().getMaxConnections}")
     logger.info(s"peerGroup.getMinBroadcastConnections==${peerGroup().getMinBroadcastConnections}")
+    logger.info(s"peerGroup.getMinBroadcastConnections==${peerGroup().getMinBroadcastConnections}")
+
+//    setDownloadListener(new DownloadProgressTracker {
+//      override def doneDownload(): Unit = {
+//        super.doneDownload()
+//        // may be called multiple times
+//        syncedPromise.trySuccess(true)
+//      }
+//    })
+
+    // we set the blockcount to the previous stored block height
+    updateBlockCount(chain().getBestChainHeight)
 
     // as soon as we are connected the peers will tell us their current height and we will advertise it immediately
     peerGroup().addConnectedEventListener(new PeerConnectedEventListener {
-      override def onPeerConnected(peer: Peer, peerCount: Int): Unit = {
-        val blockCount = peerGroup().getMostCommonChainHeight
-        // we wait for at least 3 peers before relying on current block height, but we trust localhost
-        if ((peer.getAddress.getAddr.isLoopbackAddress || peerCount > 3) && Globals.blockCount.get() < blockCount) {
-          logger.info(s"current blockchain height=$blockCount")
-          system.eventStream.publish(CurrentBlockCount(blockCount))
-          Globals.blockCount.set(blockCount)
+      override def onPeerConnected(peer: Peer, peerCount: Int): Unit =
+      // we wait for at least 3 peers before relying on the information they are giving, but we trust localhost
+        if (peer.getAddress.getAddr.isLoopbackAddress || peerCount > 3) {
+          updateBlockCount(peerGroup().getMostCommonChainHeight)
+          // may be called multiple times
+          atCurrentHeightPromise.trySuccess(true)
         }
-      }
-    })
-
-    peerGroup().addOnTransactionBroadcastListener(new OnTransactionBroadcastListener {
-      override def onTransaction(peer: Peer, t: BitcoinjTransaction): Unit = {
-        logger.info(s"txid=${t.getHashAsString} confidence=${t.getConfidence}")
-      }
     })
 
     chain().addNewBestBlockListener(new NewBestBlockListener {
-      override def notifyNewBestBlock(storedBlock: StoredBlock): Unit = {
-        // when synchronizing we don't want to advertise previous blocks
-        if (Globals.blockCount.get() < storedBlock.getHeight) {
-          logger.debug(s"new block height=${storedBlock.getHeight} hash=${storedBlock.getHeader.getHashAsString}")
-          system.eventStream.publish(CurrentBlockCount(storedBlock.getHeight))
-          Globals.blockCount.set(storedBlock.getHeight)
-        }
-      }
+      override def notifyNewBestBlock(storedBlock: StoredBlock): Unit =
+        updateBlockCount(storedBlock.getHeight)
     })
 
     wallet().addTransactionConfidenceEventListener(new TransactionConfidenceEventListener {
@@ -74,14 +85,6 @@ class BitcoinjKit(chain: String, datadir: File)(implicit system: ActorSystem) ex
         }
         system.eventStream.publish(NewConfidenceLevel(tx, 0, depthInBlocks))
       }
-    })
-
-    wallet.addScriptsChangeEventListener(new ScriptsChangeEventListener {
-
-      import scala.collection.JavaConversions._
-
-      override def onScriptsChanged(wallet: Wallet, scripts: util.List[Script], isAddingScripts: Boolean): Unit =
-        logger.info(s"watching scripts: ${scripts.map(_.getProgram).map(BinaryData(_)).mkString(",")}")
     })
 
     initializedPromise.success(true)
