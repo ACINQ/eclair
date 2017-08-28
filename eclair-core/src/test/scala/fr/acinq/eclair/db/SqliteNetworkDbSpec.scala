5d8bc2a73bf26fea20292bd0c9dc10d3d56bbf04
pm47 <pm.padiou@gmail.com>
2017-08-28 15:49:01 +0200

    added tests to peers and channels db

--- eclair-core/src/test/scala/fr/acinq/eclair/db/SqliteNetworkDbSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/db/SqliteNetworkDbSpec.scala
@@ -31,13 +31,13 @@ class SqliteNetworkDbSpec extends FunSuite {
     val node_2 = Announcements.makeNodeAnnouncement(randomKey, "node-bob", (100.toByte, 200.toByte, 300.toByte), new InetSocketAddress(InetAddress.getByAddress(Array[Byte](192.toByte, 168.toByte, 1.toByte, 42.toByte)), 42000) :: Nil)
     val node_3 = Announcements.makeNodeAnnouncement(randomKey, "node-charlie", (100.toByte, 200.toByte, 300.toByte), new InetSocketAddress(InetAddress.getByAddress(Array[Byte](192.toByte, 168.toByte, 1.toByte, 42.toByte)), 42000) :: Nil)
 
-    assert(db.listNodes().size === 0)
+    assert(db.listNodes().toSet === Set.empty)
     db.addNode(node_1)
     db.addNode(node_2)
     db.addNode(node_3)
-    assert(db.listNodes().size === 3)
+    assert(db.listNodes().toSet === Set(node_1, node_2, node_3))
     db.removeNode(node_2.nodeId)
-    assert(db.listNodes().size === 2)
+    assert(db.listNodes().toSet === Set(node_1, node_3))
     db.updateNode(node_1)
   }
 
@@ -51,25 +51,25 @@ class SqliteNetworkDbSpec extends FunSuite {
     val channel_2 = Announcements.makeChannelAnnouncement(43, randomKey.publicKey, randomKey.publicKey, randomKey.publicKey, randomKey.publicKey, sig, sig, sig, sig)
     val channel_3 = Announcements.makeChannelAnnouncement(44, randomKey.publicKey, randomKey.publicKey, randomKey.publicKey, randomKey.publicKey, sig, sig, sig, sig)
 
-    assert(db.listChannels().size === 0)
+    assert(db.listChannels().toSet === Set.empty)
     db.addChannel(channel_1)
     db.addChannel(channel_2)
     db.addChannel(channel_3)
-    assert(db.listChannels().size === 3)
+    assert(db.listChannels().toSet === Set(channel_1, channel_2, channel_3))
     db.removeChannel(channel_2.shortChannelId)
-    assert(db.listChannels().size === 2)
+    assert(db.listChannels().toSet === Set(channel_1, channel_3))
 
     val channel_update_1 = Announcements.makeChannelUpdate(randomKey, randomKey.publicKey, 42, 5, 7000000, 50000, 100, true)
     val channel_update_2 = Announcements.makeChannelUpdate(randomKey, randomKey.publicKey, 43, 5, 7000000, 50000, 100, true)
     val channel_update_3 = Announcements.makeChannelUpdate(randomKey, randomKey.publicKey, 44, 5, 7000000, 50000, 100, true)
 
-    assert(db.listChannelUpdates().size === 0)
+    assert(db.listChannelUpdates().toSet === Set.empty)
     db.addChannelUpdate(channel_update_1)
     intercept[SQLiteException](db.addChannelUpdate(channel_update_2))
     db.addChannelUpdate(channel_update_3)
     db.removeChannel(channel_3.shortChannelId)
-    assert(db.listChannels().size === 1)
-    assert(db.listChannelUpdates().size === 1)
+    assert(db.listChannels().toSet === Set(channel_1))
+    assert(db.listChannelUpdates().toSet === Set(channel_update_1))
     db.updateChannelUpdate(channel_update_1)
   }
 
