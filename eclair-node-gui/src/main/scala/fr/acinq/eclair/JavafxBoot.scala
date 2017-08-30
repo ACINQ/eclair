a066e0d042a9c04d0adbfdaa3af525d8528ac646
Fabrice Drouin <sstone@users.noreply.github.com>
2017-08-30 13:42:58 +0200

    Switch to java environment variables instead of command line options (#149)
    * added an eclair.printToconsole is defined log to stdout
    * datadir is now eclair.datadir if defined, or user.home/.eclair
    * update README.md

--- eclair-node-gui/src/main/scala/fr/acinq/eclair/JavafxBoot.scala
+++ eclair-node-gui/src/main/scala/fr/acinq/eclair/JavafxBoot.scala
@@ -10,25 +10,15 @@ import grizzled.slf4j.Logging
   * Created by PM on 25/01/2016.
   */
 object JavafxBoot extends App with Logging {
-
-  case class CmdLineConfig(datadir: File = new File(System.getProperty("user.home"), ".eclair"), headless: Boolean = false)
-
-  val parser = new scopt.OptionParser[CmdLineConfig]("eclair") {
-    head("eclair gui", s"${getClass.getPackage.getImplementationVersion} (commit: ${getClass.getPackage.getSpecificationVersion})")
-    help("help").abbr("h").text("display usage text")
-    opt[File]("datadir").optional().valueName("<file>").action((x, c) => c.copy(datadir = x)).text("optional data directory, default is ~/.eclair")
-    opt[Unit]("headless").optional().action((_, c) => c.copy(headless = true)).text("runs eclair without a gui")
-  }
+  val datadir = new File(System.getProperty("eclair.datadir", System.getProperty("user.home") + "/.eclair"))
 
   try {
-    parser.parse(args, CmdLineConfig()) match {
-      case Some(config) if config.headless =>
-        LogSetup.logTo(config.datadir)
-        new Setup(config.datadir).bootstrap
-      case Some(config) =>
-        LogSetup.logTo(config.datadir)
-        LauncherImpl.launchApplication(classOf[FxApp], classOf[FxPreloader], Array(config.datadir.getAbsolutePath))
-      case None => System.exit(0)
+    val headless = System.getProperty("eclair.headless") != null
+
+    if (headless) {
+      new Setup(datadir).bootstrap
+    } else {
+      LauncherImpl.launchApplication(classOf[FxApp], classOf[FxPreloader], Array(datadir.getAbsolutePath))
     }
   } catch {
     case t: Throwable =>
