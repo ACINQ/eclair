06286821c4fcebfd6d13b936af5698675ef9ec96
Andrea Raspitzu <a.raspitzu@gmail.com>
2018-01-10 15:50:01 +0700

    Add test for "peers" RPC call

--- eclair-core/src/test/scala/fr/acinq/eclair/api/JsonRpcServiceSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/api/JsonRpcServiceSpec.scala
@@ -1,51 +1,55 @@
 package fr.acinq.eclair.api
 
-import akka.actor.{Actor, Props}
+import akka.actor.Actor.Receive
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import org.scalatest.{FunSuite, FunSuiteLike}
 import akka.http.scaladsl.model.{HttpRequest, StatusCodes}
-import akka.http.scaladsl.testkit.ScalatestRouteTest
-import akka.testkit.TestKit
-import de.heikoseeberger.akkahttpjson4s.Json4sSupport
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import de.heikoseeberger.akkahttpjson4s.Json4sSupport.{marshaller, unmarshaller}
 import fr.acinq.eclair.blockchain.TestWallet
 import fr.acinq.eclair.{Kit, TestConstants}
+import TestConstants._
+import fr.acinq.eclair.io.Peer.{GetPeerInfo, PeerInfo}
 
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
   
-  def mockService = new Service {
-    
-    override def getInfoResponse: Future[GetInfoResponse] = Future.successful(???)
+  implicit def default(implicit system: ActorSystem) = RouteTestTimeout(1 second)
   
-    override def appKit: Kit = Kit(
-      nodeParams = TestConstants.Alice.nodeParams,
-      system = system,
-      watcher = system.actorOf(Props(new MockActor)),
-      paymentHandler =  system.actorOf(Props(new MockActor)),
-      register =  system.actorOf(Props(new MockActor)),
-      relayer =  system.actorOf(Props(new MockActor)),
-      router = system.actorOf(Props(new MockActor)),
-      switchboard =  system.actorOf(Props(new MockActor)),
-      paymentInitiator =  system.actorOf(Props(new MockActor)),
-      server =  system.actorOf(Props(new MockActor)),
-      wallet = new TestWallet
-    )
-  }
+  def defaultMockKit = Kit(
+    nodeParams = Alice.nodeParams,
+    system = system,
+    watcher = system.actorOf(Props(new MockActor)),
+    paymentHandler =  system.actorOf(Props(new MockActor)),
+    register =  system.actorOf(Props(new MockActor)),
+    relayer =  system.actorOf(Props(new MockActor)),
+    router = system.actorOf(Props(new MockActor)),
+    switchboard =  system.actorOf(Props(new MockActor)),
+    paymentInitiator =  system.actorOf(Props(new MockActor)),
+    server =  system.actorOf(Props(new MockActor)),
+    wallet = new TestWallet
+  )
   
   class MockActor extends Actor {
     override def receive: Receive = {
       case msg =>
-        println(s"mock actor got $msg")
-        sender() ! "Yo"
     }
   }
   
-  implicit val formats = mockService.formats
-  implicit val ser = mockService.serialization
-  import Json4sSupport.{marshaller, unmarshaller}
+  class MockService(kit: Kit = defaultMockKit) extends Service {
+    override def getInfoResponse: Future[GetInfoResponse] = Future.successful(???)
+    override def appKit: Kit = kit
+  }
+  
   
   test("Calling non root path should result in HTTP 404") {
   
+    val mockService = new MockService
+    import mockService.formats
+    import mockService.serialization
+    
     val postBody = JsonRPCBody(
       method = "help",
       params = Seq.empty
@@ -61,7 +65,10 @@ class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
   }
   
   test("Help should respond with a help message") {
-    
+    val mockService = new MockService
+    import mockService.formats
+    import mockService.serialization
+  
     val postBody = JsonRPCBody(
       method = "help",
       params = Seq.empty
@@ -78,6 +85,46 @@ class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
     }
     
     
+  }
+  
+  test("Peers should ask the switchboard for current known peers") {
+    
+    val mockAlicePeer = system.actorOf(Props(new {} with MockActor {
+      override def receive = {
+        case GetPeerInfo => sender() ! PeerInfo(
+          nodeId = Alice.id,
+          state = "CONNECTED",
+          address = None,
+          channels = 1)
+      }
+    }))
+    val mockService = new MockService(defaultMockKit.copy(
+      switchboard = system.actorOf(Props(new {} with MockActor {
+        override def receive = {
+          case 'peers => sender() ! Map(Alice.id -> mockAlicePeer)
+        }
+      }))
+    ))
+    
+    import mockService.formats
+    import mockService.serialization
+    
+    val postBody = JsonRPCBody(
+      method = "peers",
+      params = Seq.empty
+    )
+    
+    Post("/", postBody) ~>
+      addHeader("Content-Type", "application/json") ~>
+      mockService.route ~>
+      check {
+        assert(handled)
+        assert(status == StatusCodes.OK)
+        val peerInfos = entityAs[JsonRPCRes].result.asInstanceOf[Seq[Map[String,String]]]
+        assert(peerInfos.size == 1)
+        assert(peerInfos.head.get("nodeId") == Some(Alice.id.toString))
+        assert(peerInfos.head.get("state") == Some("CONNECTED"))
+      }
   }
   
 }
