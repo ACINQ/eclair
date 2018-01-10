cbbf6bb153654c40d44c4cbb26dfecfe12a9b375
sstone <sstone@users.noreply.github.com>
2018-01-10 14:39:21 +0100

    Fix encoding of failure messages (fixes #363) When a ailure message includes a channel update field, this field must be encoded as: [ len | channel update] And we did not include the len field.

--- eclair-core/src/test/scala/fr/acinq/eclair/wire/FailureMessageLightningMessageCodecsSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/wire/FailureMessageLightningMessageCodecsSpec.scala
@@ -1,6 +1,6 @@
 package fr.acinq.eclair.wire
 
-import fr.acinq.bitcoin.BinaryData
+import fr.acinq.bitcoin.{BinaryData, Block}
 import org.junit.runner.RunWith
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
@@ -12,6 +12,16 @@ import scala.util.Random
   */
 @RunWith(classOf[JUnitRunner])
 class FailureMessageLightningMessageCodecsSpec extends FunSuite {
+  val channelUpdate = ChannelUpdate(
+    signature = BinaryData("3045022100c451cd65c88f55b1767941a247e849e12f5f4d4a93a07316659e22f5267d2088022009042a595c6bc8942cd9d729317b82b306edc259fb6b3a3cecb3dd1bd446e90601"),
+    chainHash = Block.RegtestGenesisBlock.hash,
+    shortChannelId = 12345,
+    timestamp = 1234567L,
+    cltvExpiryDelta = 100,
+    flags = BinaryData("0001"),
+    htlcMinimumMsat = 1000,
+    feeBaseMsat = 12,
+    feeProportionalMillionths = 76)
 
   def randomBytes(size: Int): BinaryData = {
     val bin = new Array[Byte](size)
@@ -20,14 +30,12 @@ class FailureMessageLightningMessageCodecsSpec extends FunSuite {
   }
 
   test("encode/decode all channel messages") {
-
-    val invalidRealm = InvalidRealm
-    val temporaryNodeFailure = TemporaryNodeFailure
-    val permanentNodeFailure = PermanentNodeFailure
-
-
     val msgs: List[FailureMessage] =
-      invalidRealm :: temporaryNodeFailure :: permanentNodeFailure :: Nil
+      InvalidRealm :: TemporaryNodeFailure :: PermanentNodeFailure :: RequiredNodeFeatureMissing ::
+        InvalidOnionVersion(randomBytes(32)) :: InvalidOnionHmac(randomBytes(32)) :: InvalidOnionKey(randomBytes(32)) ::
+        TemporaryChannelFailure(channelUpdate) :: PermanentChannelFailure :: RequiredChannelFeatureMissing :: UnknownNextPeer ::
+        AmountBelowMinimum(123456, channelUpdate) :: FeeInsufficient(546463, channelUpdate) :: IncorrectCltvExpiry(1211, channelUpdate) :: ExpiryTooSoon(channelUpdate)  ::
+        UnknownPaymentHash :: IncorrectPaymentAmount :: FinalExpiryTooSoon :: FinalIncorrectCltvExpiry(1234) :: ChannelDisabled(BinaryData("0101"), channelUpdate) :: Nil
 
     msgs.foreach {
       case msg => {
@@ -37,5 +45,4 @@ class FailureMessageLightningMessageCodecsSpec extends FunSuite {
       }
     }
   }
-
 }
