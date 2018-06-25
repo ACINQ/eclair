e957a6cdb9ec065d0848e8c28d3c0cdd4cf27955
araspitzu <a.raspitzu@gmail.com>
2018-06-25 16:09:49 +0200

    use mock responses in test (check against actual response)

--- eclair-core/src/test/scala/fr/acinq/eclair/api/JsonRpcServiceSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/api/JsonRpcServiceSpec.scala
@@ -1,5 +1,8 @@
 package fr.acinq.eclair.api
 
+import java.nio.charset.StandardCharsets._
+import java.nio.file.{Files, Path, Paths, StandardOpenOption}
+import java.nio.file.StandardOpenOption._
 import akka.actor.{Actor, ActorSystem, Props, Scheduler}
 import org.scalatest.FunSuite
 import akka.http.scaladsl.model.StatusCodes
@@ -14,9 +17,15 @@ import akka.http.scaladsl.model.headers.BasicHttpCredentials
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Route
 import akka.stream.scaladsl.Flow
+import org.json4s.Formats
+import org.json4s.jackson.Serialization
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
+@RunWith(classOf[JUnitRunner])
 class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
   
   //a WARN is being thrown by akka, currently there is an open issue to test the
@@ -45,6 +54,7 @@ class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
   
   class MockService(kit: Kit = defaultMockKit) extends Service {
     override def getInfoResponse: Future[GetInfoResponse] = Future.successful(???)
+
     override def appKit: Kit = kit
 
     override val scheduler: Scheduler = system.scheduler
@@ -68,8 +78,8 @@ class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
       check {
         assert(handled)
         assert(status == StatusCodes.OK)
-        val helpList = entityAs[JsonRPCRes].result.asInstanceOf[List[String]]
-        assert(helpList.mkString == mockService.help.mkString)
+        val resp = entityAs[JsonRPCRes]
+        matchTestJson("help", false ,resp)
     }
     
     
@@ -106,11 +116,32 @@ class JsonRpcServiceSpec extends FunSuite with ScalatestRouteTest {
       check {
         assert(handled)
         assert(status == StatusCodes.OK)
-        val peerInfos = entityAs[JsonRPCRes].result.asInstanceOf[Seq[Map[String,String]]]
+        val response = entityAs[JsonRPCRes]
+        val peerInfos = response.result.asInstanceOf[Seq[Map[String,String]]]
         assert(peerInfos.size == 1)
         assert(peerInfos.head.get("nodeId") == Some(Alice.nodeParams.nodeId.toString))
         assert(peerInfos.head.get("state") == Some("CONNECTED"))
+        matchTestJson("peers", false, response)
       }
   }
-  
+
+
+  def readFileAsString(path: Path): String = Files.exists(path) match {
+    case true => new String(Files.readAllBytes(path.toAbsolutePath))
+    case false => throw new IllegalArgumentException(s"Mock file for $path does not exist, please use 'overWrite' first.")
+  }
+
+  def matchTestJson(rpcMethod: String, overWrite: Boolean, response: JsonRPCRes)(implicit formats: Formats) = {
+    val responseContent = Serialization.writePretty(response)
+    val path = Paths.get(s"src/test/resources/api/$rpcMethod")
+    if(overWrite){
+      Files.write(path, responseContent.getBytes(UTF_8), TRUNCATE_EXISTING, CREATE)
+      assert(false, "'overWrite' should be false before commit")
+    }else{
+      val expectedResponse = readFileAsString(path)
+      assert(responseContent == expectedResponse)
+    }
+
+  }
+
 }
