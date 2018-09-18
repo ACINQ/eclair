99b901d1d9a32c8265c8a757e0300831224801a8
sstone <fabrice.drouin@acinq.fr>
2018-09-18 13:54:38 +0200

    fix pruning test

--- eclair-core/src/test/scala/fr/acinq/eclair/router/PruningSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/router/PruningSpec.scala
@@ -99,7 +99,6 @@ class PruningSpec extends TestkitBaseClass with BeforeAndAfterAll {
       val shortChannelIds1 = shortChannelIds.drop(10)
       val reply = ReplyChannelRange(chainHash, firstBlockNum, numberOfBlocks, 1.toByte, ChannelRangeQueries.encodeShortChannelIdsSingle(shortChannelIds1, ChannelRangeQueries.ZLIB_FORMAT, false))
       probe.send(router, PeerRoutingMessage(probe.ref, remoteNodeId, reply))
-      probe.expectMsgType[QueryShortChannelIds]
 
       // router should see that it has 10 channels that we don't have, check if they're stale, and prune them
       awaitCond({
