c24732171993486a9ee07c7909a87daca602c47e
Pierre-Marie Padiou <pm47@users.noreply.github.com>
2019-01-03 16:11:10 +0100

    Update eclair-core/src/main/scala/fr/acinq/eclair/channel/Channel.scala
    Update comment on max refund delay for the commitment tx (CLTV)
    
    Co-Authored-By: araspitzu <a.raspitzu@protonmail.com>

--- eclair-core/src/main/scala/fr/acinq/eclair/channel/Channel.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/channel/Channel.scala
@@ -62,7 +62,7 @@ object Channel {
   val MIN_CLTV_EXPIRY = 9L
   val MAX_CLTV_EXPIRY = 7 * 144L // one week
 
-  //  since BOLTv1.1
+  // since BOLT 1.1, there is a max value for the refund delay of the main commitment tx
   val MAX_MAIN_CLTV = 2016L
 
   val REFRESH_CHANNEL_UPDATE_INTERVAL = 7 days
