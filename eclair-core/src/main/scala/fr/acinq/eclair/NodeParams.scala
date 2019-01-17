75e10e305f99eed27b755f630d5df679e0694b19
Andrea <andrea.raspitzu@acinq.fr>
2019-01-17 11:50:17 +0100

    WIP fix alias length with utf-8 encoding

--- eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
@@ -179,7 +179,7 @@ object NodeParams {
 
     NodeParams(
       keyManager = keyManager,
-      alias = config.getString("node-alias").take(32),
+      alias = new String(config.getString("node-alias").getBytes("UTF-8").take(32)),
       color = Color(color.data(0), color.data(1), color.data(2)),
       publicAddresses = config.getStringList("server.public-ips").toList.map(ip => new InetSocketAddress(InetAddresses.forString(ip), config.getInt("server.port"))),
       globalFeatures = BinaryData(config.getString("global-features")),
