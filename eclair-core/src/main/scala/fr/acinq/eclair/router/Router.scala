64b8b3ddc2e71bd5fa01326bbc5ae1d5f2c8e978
Andrea <andrea.raspitzu@acinq.fr>
2019-01-28 15:55:23 +0100

    Do correct percentage comparison in router (boundaries feature)

--- eclair-core/src/main/scala/fr/acinq/eclair/router/Router.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/router/Router.scala
@@ -824,7 +824,7 @@ object Router {
     val currentBlockHeight = Globals.blockCount.get()
 
     val ensureFeeCap: CompoundWeight => Boolean = { cp =>
-      cp.rawCost < maxFeeBaseMsat || (cp.rawCost * 100D) / amountMsat < maxFeePct
+      cp.rawCost < maxFeeBaseMsat || cp.rawCost.toDouble < maxFeePct * amountMsat
     }
 
     val foundRoutes = Graph.yenKshortestPaths(g, localNodeId, targetNodeId, amountMsat, ignoredEdges, extraEdges, numRoutes, wr, currentBlockHeight, ensureFeeCap).toList match {
