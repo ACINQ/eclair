3ab935c4688b56f42297948b77f12742d12e4e28
pm47 <pm.padiou@gmail.com>
2019-02-12 21:26:51 +0100

    don't verify public keys loaded from network.db

--- eclair-core/src/main/scala/fr/acinq/eclair/db/sqlite/SqliteNetworkDb.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/db/sqlite/SqliteNetworkDb.scala
@@ -108,8 +108,8 @@ class SqliteNetworkDb(sqlite: Connection) extends NetworkDb {
           features = null,
           chainHash = null,
           shortChannelId = ShortChannelId(rs.getLong("short_channel_id")),
-          nodeId1 = PublicKey(PublicKey(rs.getBytes("node_id_1")).value, compressed = true), // we read as uncompressed, and convert to compressed
-          nodeId2 = PublicKey(PublicKey(rs.getBytes("node_id_2")).value, compressed = true),
+          nodeId1 = PublicKey(PublicKey(rs.getBytes("node_id_1"), checkValid = false).value, compressed = true), // we read as uncompressed, and convert to compressed, and we don't check the validity it was already checked before
+          nodeId2 = PublicKey(PublicKey(rs.getBytes("node_id_2"), checkValid = false).value, compressed = true),
           bitcoinKey1 = null,
           bitcoinKey2 = null) -> (emptyTxid, zeroCapacity))
       }
