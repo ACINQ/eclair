1ca807fdeee43c1a6979d7fa1d8b683688b56dc5
Andrea <andrea.raspitzu@acinq.fr>
2019-03-20 16:23:32 +0100

    Add test for normal operation and heuristic operation

--- eclair-core/src/test/scala/fr/acinq/eclair/router/FuzzyGraph.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/router/FuzzyGraph.scala
@@ -3,66 +3,57 @@ package fr.acinq.eclair.router
 import java.nio.file.Paths
 import java.sql.DriverManager
 
-import fr.acinq.bitcoin.ByteVector32
+import fr.acinq.bitcoin.{ByteVector32, MilliSatoshi}
 import fr.acinq.bitcoin.Crypto.PublicKey
 import fr.acinq.eclair.ShortChannelId
-import fr.acinq.eclair.channel.Channel
 import fr.acinq.eclair.db.sqlite.SqliteNetworkDb
 import fr.acinq.eclair.router.Graph.GraphStructure.{DirectedGraph, GraphEdge}
 import fr.acinq.eclair.router.Graph.WeightRatios
 import fr.acinq.eclair.router.Router.getDesc
 import fr.acinq.eclair.wire.{ChannelAnnouncement, ChannelUpdate}
-import org.scalatest.{Outcome, fixture}
+import org.scalatest.FunSuite
 
 import scala.collection.immutable.TreeMap
 import scala.util.{Failure, Random, Success}
 import scodec.bits._
 
 
-class FuzzyGraph extends fixture.FunSuite {
+class FuzzyGraph extends FunSuite {
 
-  val DEFAULT_ROUTE_PARAMS = RouteParams(randomize = false, maxFeeBaseMsat = 21000, maxFeePct = 0.03, routeMaxCltv = 2016, routeMaxLength = 6, ratios = Some(WeightRatios(
+  val DEFAULT_ROUTE_PARAMS = RouteParams(randomize = false, maxFeeBaseMsat = 21000, maxFeePct = 0.03, routeMaxCltv = 2016, routeMaxLength = 8, ratios = Some(WeightRatios(
     cltvDeltaFactor = 0.15, ageFactor = 0.35, capacityFactor = 0.5
   )))
 
-  case class FixtureParam(networkData: Map[ChannelDesc, ChannelUpdate])
+  val AMOUNT_TO_ROUTE = MilliSatoshi(1000000).toLong // 1000sat
 
-  override def withFixture(test: OneArgTest): Outcome = {
-    val networkDbFile = Paths.get("eclair-core/src/test/resources/network-db-mainnet-20032019.sqlite").toFile
-    assert(networkDbFile.exists() && networkDbFile.canRead)
-    println(s"${networkDbFile.getAbsolutePath}")
+  val networkDbFile = Paths.get("eclair-core/src/test/resources/network-db-mainnet-20032019.sqlite").toFile
+  val dbConnection = DriverManager.getConnection(s"jdbc:sqlite:$networkDbFile")
+  val db = new SqliteNetworkDb(dbConnection)
 
-    val dbConnection = DriverManager.getConnection(s"jdbc:sqlite:$networkDbFile")
-    val db = new SqliteNetworkDb(dbConnection)
+  val channels = db.listChannels()
+  val updates = db.listChannelUpdates()
+  val initChannels = channels.keys.foldLeft(TreeMap.empty[ShortChannelId, ChannelAnnouncement]) { case (m, c) => m + (c.shortChannelId -> c) }
+  val initChannelUpdates = updates.map { u =>
+    val desc = getDesc(u, initChannels(u.shortChannelId))
+    desc -> u
+  }.toMap
+  println("Test ready")
 
-    val channels = db.listChannels()
-    val updates = db.listChannelUpdates()
-    val initChannels = channels.keys.foldLeft(TreeMap.empty[ShortChannelId, ChannelAnnouncement]) { case (m, c) => m + (c.shortChannelId -> c) }
-    val initChannelUpdates = updates.map { u =>
-      val desc = getDesc(u, initChannels(u.shortChannelId))
-      desc -> u
-    }.toMap
+  test("find 200 paths between random nodes in the graph") {
 
-    super.withFixture(test.toNoArgTest(FixtureParam(initChannelUpdates)))
-  }
-
-  test("find a path between two random nodes in the graph") { fixtureParam =>
-
-    val g = DirectedGraph.makeGraph(fixtureParam.networkData)
+    val g = DirectedGraph.makeGraph(initChannelUpdates)
     val nodes = g.vertexSet().toList
-    println(s"Nodes: ${nodes.size}")
 
-    for(i <- 0 to 100) {
+    for(i <- 0 until 200) {
+      if(i % 10 == 0) println(s"Iteration: $i")
 
-      val randomSource = nodes(Random.nextInt(nodes.size))  // PublicKey(hex"02feac67caa47b1d718c685c885d062b7eb36383295ed701a476af503306691884")
-      val randomTarget = nodes(Random.nextInt(nodes.size))  // PublicKey(hex"03b6a0e7cb7b2829384fc3ac95eeb3f19c71334d9ab32381572bf03b9313c66db7")
+      val randomSource = nodes(Random.nextInt(nodes.size))
+      val randomTarget = nodes(Random.nextInt(nodes.size))
 
-      println(s"[$i]   ${randomSource.toString()}  --->   ${randomTarget.toString()}")
-
-      val fallbackRoute = connectNodes(randomSource, randomTarget, g, nodes)
+      val fallbackRoute = connectNodes(randomSource, randomTarget, g, nodes, length = 8)
       val g1 = fallbackRoute.foldLeft(g)((acc, edge) => acc.addEdge(edge))
 
-      Router.findRoute(g1, randomSource, randomTarget, 1000000, 0, Set.empty, Set.empty, DEFAULT_ROUTE_PARAMS) match {
+      Router.findRoute(g1, randomSource, randomTarget, AMOUNT_TO_ROUTE, 0, Set.empty, Set.empty, DEFAULT_ROUTE_PARAMS) match {
         case Failure(exception) => throw exception
         case Success(route) =>
           if(route.map(_.lastUpdate.shortChannelId) == fallbackRoute.map(_.desc.shortChannelId)){
@@ -75,14 +66,66 @@ class FuzzyGraph extends fixture.FunSuite {
     true
   }
 
-  private def connectNodes(source: PublicKey, target: PublicKey, g: DirectedGraph, nodes: List[PublicKey], length: Int = 4): Seq[GraphEdge] = {
+  test("find 200 paths using random heuristics weight") {
 
-    (0 until length).map { i =>
-      val from = if(i == 0) source else nodes(Random.nextInt(nodes.size))
-      val to = if(i == length - 1) target else nodes(Random.nextInt(nodes.size))
+    val g = DirectedGraph.makeGraph(initChannelUpdates)
+    val nodes = g.vertexSet().toList
+
+    for(i <- 0 until 200) {
+      if(i % 10 == 0) println(s"Iteration: $i")
+
+      val randomSource = nodes(Random.nextInt(nodes.size))
+      val randomTarget = nodes(Random.nextInt(nodes.size))
+
+      val fallbackRoute = connectNodes(randomSource, randomTarget, g, nodes, length = 8)
+      val g1 = fallbackRoute.foldLeft(g)((acc, edge) => acc.addEdge(edge))
+
+      val x1 = (Random.nextInt(97) + 1) / 100D
+      val x2 = (Random.nextInt(97) + 1) / 100D
+
+      val high = math.max(x1, x2)
+      val low = if(high == x1) x2 else x1
+
+      val wr = WeightRatios(
+        cltvDeltaFactor = low,
+        ageFactor = high - low,
+        capacityFactor = 1 - high
+      )
+
+      Router.findRoute(g1, randomSource, randomTarget, 1000000, 0, Set.empty, Set.empty, DEFAULT_ROUTE_PARAMS.copy(ratios = Some(wr))) match {
+        case Failure(exception) => throw exception
+        case Success(route) =>
+          if(route.map(_.lastUpdate.shortChannelId) == fallbackRoute.map(_.desc.shortChannelId)){
+            println(s"Using fallback route!")
+          }
+      }
+    }
+    true
+  }
+
+  /**
+    * Creates an arbitraty long path that connects source -> ... -> target through random nodes. Conjunction channels will have
+    * high fee, low capacity, recent-aged and higher than average cltv
+    *
+    * @param source
+    * @param target
+    * @param g
+    * @param nodes
+    * @param length
+    * @return
+    */
+  private def connectNodes(source: PublicKey, target: PublicKey, g: DirectedGraph, nodes: List[PublicKey], length: Int = 6): Seq[GraphEdge] = {
+
+    val iterations = 0 until length
+    // pick 'length' conjunctions and add source/target at the beginning/end
+    val randomNodes = source +: iterations.drop(2).map(_ => nodes(Random.nextInt(nodes.size))) :+ target
+
+    iterations.dropRight(1).map { i =>
+      val from = randomNodes(i)
+      val to = randomNodes(i + 1)
 
       val shortChannelId = ShortChannelId(
-        blockHeight = 600000 + Random.nextInt(90000),
+        blockHeight = 600000 + Random.nextInt(90000),  // height from 600k onward, those channels will be younger than the rest
         txIndex = Random.nextInt(2000),
         0
       )
@@ -95,14 +138,12 @@ class FuzzyGraph extends fixture.FunSuite {
         timestamp = 0,
         messageFlags = 1,
         channelFlags = 0,
-        cltvExpiryDelta = 144,
+        cltvExpiryDelta = 244,
         htlcMinimumMsat = 0,
-        htlcMaximumMsat = Some(16000000),
-        feeBaseMsat = 10,
-        feeProportionalMillionths = 1
+        htlcMaximumMsat = Some(AMOUNT_TO_ROUTE * 3),
+        feeBaseMsat = 100,
+        feeProportionalMillionths = 10
       )
-
-      println(s" ADDING AN EDGE ${from.toString().take(8)}  -->  ${to.toString().take(8)}")
 
       GraphEdge(desc, update)
     }
