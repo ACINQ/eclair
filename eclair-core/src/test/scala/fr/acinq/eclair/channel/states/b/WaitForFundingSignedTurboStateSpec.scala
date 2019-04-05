604d60e1c7ef5e559e8148441dcff858ffb73c89
anton <admin@btcontract.com>
2019-04-05 09:49:58 +0300

    Support turbo channels in funder mode
    # Conflicts:
    #	eclair-core/src/main/scala/fr/acinq/eclair/channel/Channel.scala

--- eclair-core/src/test/scala/fr/acinq/eclair/channel/states/b/WaitForFundingSignedTurboStateSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/channel/states/b/WaitForFundingSignedTurboStateSpec.scala
@@ -17,7 +17,7 @@
 package fr.acinq.eclair.channel.states.b
 
 import akka.testkit.{TestFSMRef, TestProbe}
-import fr.acinq.bitcoin.BinaryData
+import fr.acinq.bitcoin.ByteVector32
 import fr.acinq.eclair.TestConstants.{Alice, Bob}
 import fr.acinq.eclair.blockchain._
 import fr.acinq.eclair.channel._
@@ -25,6 +25,7 @@ import fr.acinq.eclair.channel.states.StateTestsHelperMethods
 import fr.acinq.eclair.wire._
 import fr.acinq.eclair.{TestConstants, TestkitBaseClass}
 import org.scalatest.Outcome
+import scodec.bits.ByteVector
 
 import scala.concurrent.duration._
 
@@ -39,8 +40,8 @@ class WaitForFundingSignedTurboStateSpec extends TestkitBaseClass with StateTest
     val aliceInit = Init(Alice.channelParams.globalFeatures, Alice.channelParams.localFeatures)
     val bobInit = Init(Bob.channelParams.globalFeatures, Bob.channelParams.localFeatures)
     within(30 seconds) {
-      alice ! INPUT_INIT_FUNDER("00" * 32, TestConstants.fundingSatoshis, TestConstants.pushMsat, TestConstants.feeratePerKw, TestConstants.feeratePerKw, Alice.channelParams, alice2bob.ref, bobInit, ChannelFlags.PrivateTurbo)
-      bob ! INPUT_INIT_FUNDEE("00" * 32, Bob.channelParams, bob2alice.ref, aliceInit)
+      alice ! INPUT_INIT_FUNDER(ByteVector32.fromValidHex("00" * 32), TestConstants.fundingSatoshis, TestConstants.pushMsat, TestConstants.feeratePerKw, TestConstants.feeratePerKw, Alice.channelParams, alice2bob.ref, bobInit, ChannelFlags.PrivateTurbo)
+      bob ! INPUT_INIT_FUNDEE(ByteVector32.fromValidHex("00" * 32), Bob.channelParams, bob2alice.ref, aliceInit)
       alice2bob.expectMsgType[OpenChannel]
       alice2bob.forward(bob)
       bob2alice.expectMsgType[AcceptChannel]
@@ -73,7 +74,7 @@ class WaitForFundingSignedTurboStateSpec extends TestkitBaseClass with StateTest
   test("recv FundingSigned with invalid signature") { f =>
     import f._
     // sending an invalid sig
-    alice ! FundingSigned("00" * 32, BinaryData("00" * 64))
+    alice ! FundingSigned(ByteVector32.fromValidHex("00" * 32), ByteVector.fromValidHex("00" * 64))
     awaitCond(alice.stateName == CLOSED)
     alice2bob.expectMsgType[Error]
   }
