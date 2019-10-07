c8b8a3e9fa328988227b85e5d1cc9ef304b9f1e9
sstone <fabrice.drouin@acinq.fr>
2019-10-07 10:44:52 +0200

    SqliteNetworkDb: add regression test
    We check that our new db schema is compatible with existing data i.e that we can read txids as TEXT when they were
    created and stored as STRING.

--- eclair-core/src/test/scala/fr/acinq/eclair/db/SqliteNetworkDbSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/db/SqliteNetworkDbSpec.scala
@@ -173,6 +173,34 @@ class SqliteNetworkDbSpec extends FunSuite {
     simpleTest(sqlite)
   }
 
+  test("non-regression test") {
+    val sqlite = TestConstants.sqliteInMemory()
+    val dbLegacy = new SqliteNetworkDbLegacy(sqlite)
+    val sig = Crypto.sign(randomBytes32, randomKey)
+    val priv = randomKey
+    val pub = priv.publicKey
+    val capacity = 10000 sat
+
+    // create channels and updates, and populate a legacy db
+    val channels = shortChannelIds.map(id => Announcements.makeChannelAnnouncement(Block.RegtestGenesisBlock.hash, id, pub, pub, pub, pub, sig, sig, sig, sig))
+    val txids = shortChannelIds.map(id => id -> randomBytes32).toMap
+    val template = Announcements.makeChannelUpdate(Block.RegtestGenesisBlock.hash, priv, pub, ShortChannelId(42), CltvExpiryDelta(5), 7000000 msat, 50000 msat, 100, 500000000L msat, true)
+    val updates = shortChannelIds.map(id => template.copy(shortChannelId = id))
+
+    channels.foreach(ca => dbLegacy.addChannel(ca, txids(ca.shortChannelId), capacity))
+    updates.foreach(u => dbLegacy.updateChannel(u))
+    val expected = {
+      val updatesMap = updates.map(u => u.shortChannelId -> u).toMap
+      SortedMap(channels.map(c => c.shortChannelId -> PublicChannel(c, txids(c.shortChannelId), capacity, None, Some(updatesMap(c.shortChannelId)))): _*)
+    }
+    assert(dbLegacy.listChannels() == expected)
+
+
+    // now check that we can read the same data with the new schema
+    val db = new SqliteNetworkDb(sqlite)
+    assert(db.listChannels() == expected)
+  }
+
   test("remove many channels") {
     val sqlite = TestConstants.sqliteInMemory()
     val db = new SqliteNetworkDb(sqlite)
