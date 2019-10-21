ec5b86c6b1afbcfd6d1b5069682351d059693466
Bastien Teinturier <bastuc@hotmail.fr>
2019-10-21 15:47:44 +0200

    Fix ignored duplicate amounts in htlc-value-in-flight Supercedes #1163 Credits to @btcontract for finding the bug

--- eclair-core/src/main/scala/fr/acinq/eclair/transactions/CommitmentSpec.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/transactions/CommitmentSpec.scala
@@ -20,8 +20,8 @@ import fr.acinq.eclair.MilliSatoshi
 import fr.acinq.eclair.wire._
 
 /**
-  * Created by PM on 07/12/2016.
-  */
+ * Created by PM on 07/12/2016.
+ */
 
 // @formatter:off
 sealed trait Direction { def opposite: Direction }
@@ -36,10 +36,10 @@ final case class CommitmentSpec(htlcs: Set[DirectedHtlc], feeratePerKw: Long, to
 }
 
 object CommitmentSpec {
-  def removeHtlc(changes: List[UpdateMessage], id: Long): List[UpdateMessage] = changes.filterNot(_ match {
+  def removeHtlc(changes: List[UpdateMessage], id: Long): List[UpdateMessage] = changes.filterNot {
     case u: UpdateAddHtlc if u.id == id => true
     case _ => false
-  })
+  }
 
   def addHtlc(spec: CommitmentSpec, direction: Direction, update: UpdateAddHtlc): CommitmentSpec = {
     val htlc = DirectedHtlc(direction, update)
@@ -54,7 +54,7 @@ object CommitmentSpec {
     spec.htlcs.find(htlc => htlc.direction != direction && htlc.add.id == htlcId) match {
       case Some(htlc) if direction == OUT => spec.copy(toLocal = spec.toLocal + htlc.add.amountMsat, htlcs = spec.htlcs - htlc)
       case Some(htlc) if direction == IN => spec.copy(toRemote = spec.toRemote + htlc.add.amountMsat, htlcs = spec.htlcs - htlc)
-      case None => throw new RuntimeException(s"cannot find htlc id=${htlcId}")
+      case None => throw new RuntimeException(s"cannot find htlc id=$htlcId")
     }
   }
 
@@ -63,7 +63,7 @@ object CommitmentSpec {
     spec.htlcs.find(htlc => htlc.direction != direction && htlc.add.id == htlcId) match {
       case Some(htlc) if direction == OUT => spec.copy(toRemote = spec.toRemote + htlc.add.amountMsat, htlcs = spec.htlcs - htlc)
       case Some(htlc) if direction == IN => spec.copy(toLocal = spec.toLocal + htlc.add.amountMsat, htlcs = spec.htlcs - htlc)
-      case None => throw new RuntimeException(s"cannot find htlc id=${htlcId}")
+      case None => throw new RuntimeException(s"cannot find htlc id=$htlcId")
     }
   }
 
