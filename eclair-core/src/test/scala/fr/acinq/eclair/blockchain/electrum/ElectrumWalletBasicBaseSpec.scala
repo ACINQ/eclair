477a10c3630feabb26ebfe0e6de00d136d8e7c40
Andrea <andrea.raspitzu@acinq.fr>
2019-10-23 15:21:16 +0200

    Parametrize ElectrumWalletBasicSpec to run in BIP49 and BIP84 mode

--- eclair-core/src/test/scala/fr/acinq/eclair/blockchain/electrum/ElectrumWalletBasicBaseSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/blockchain/electrum/ElectrumWalletBasicBaseSpec.scala
@@ -18,48 +18,38 @@ package fr.acinq.eclair.blockchain.electrum
 
 import java.sql.DriverManager
 
-import fr.acinq.bitcoin.Crypto.{PrivateKey, PublicKey}
+import fr.acinq.bitcoin.Crypto.PrivateKey
 import fr.acinq.bitcoin.DeterministicWallet.{ExtendedPrivateKey, derivePrivateKey}
 import fr.acinq.bitcoin._
+import fr.acinq.eclair.blockchain.electrum.ElectrumWallet.{NATIVE_SEGWIT, P2SH_SEGWIT}
 import fr.acinq.eclair.blockchain.electrum.db.sqlite.SqliteWalletDb
 import fr.acinq.eclair.transactions.{Scripts, Transactions}
 import grizzled.slf4j.Logging
-import org.scalatest.{FunSuite, Outcome, Tag, fixture}
+import org.scalatest.FunSuite
 import scodec.bits.ByteVector
-import scodec.bits.HexStringSyntax
 
 import scala.util.{Failure, Random, Success, Try}
 
-class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
+trait ElectrumWalletBasicBaseSpec extends FunSuite with Logging {
 
   import ElectrumWallet._
-  import ElectrumWalletBasicSpec._
+  import ElectrumWalletBasicBaseSpec._
 
-  case class FixtureParam(state: Data)
-
-  override def withFixture(test: OneArgTest): Outcome = {
-    val walletType:WalletType = test.tags.contains("bech32") match {
-      case true => NATIVE_SEGWIT
-      case false => P2SH_SEGWIT
-    }
-
-    val master = DeterministicWallet.generate(ByteVector32(ByteVector.fill(32)(1)))
-    val keyStore = walletType match {
-      case P2SH_SEGWIT => new BIP49KeyStore(master, Block.RegtestGenesisBlock.hash)
-      case NATIVE_SEGWIT => new BIP84KeyStore(master, Block.RegtestGenesisBlock.hash)
-    }
-    val firstAccountKeys = (0 until 10).map(i => keyStore.accountKey(i)).toVector
-    val firstChangeKeys = (0 until 10).map(i => keyStore.changeKey(i)).toVector
-    val state = new Data(keyStore, Blockchain.fromCheckpoints(Block.RegtestGenesisBlock.hash, CheckPoint.load(Block.RegtestGenesisBlock.hash)), firstAccountKeys, firstChangeKeys, Map(), Map(), Map(), Map(), Map(), Set(), Set(), Set(), Set(), List(), None)
-    val state1 = state.copy(status = (firstAccountKeys ++ firstChangeKeys).map(key => computeScriptHashFromScriptPubKey(state.keyStore.computePublicKeyScript(key.publicKey)) -> "").toMap)
-
-    withFixture(test.toNoArgTest(FixtureParam(state1)))
-  }
+  def walletType: WalletType
 
   val swipeRange = 10
   val dustLimit = 546 sat
   val feeRatePerKw = 20000
   val minimumFee = 2000 sat
+
+  val master = DeterministicWallet.generate(ByteVector32(ByteVector.fill(32)(1)))
+  val keyStore = walletType match {
+    case P2SH_SEGWIT => new BIP49KeyStore(master, Block.RegtestGenesisBlock.hash)
+    case NATIVE_SEGWIT => new BIP84KeyStore(master, Block.RegtestGenesisBlock.hash)
+  }
+  val params = ElectrumWallet.WalletParameters(walletType, Block.RegtestGenesisBlock.hash, new SqliteWalletDb(DriverManager.getConnection("jdbc:sqlite::memory:")))
+  val state = Data.createNew(keyStore, Blockchain.fromCheckpoints(Block.RegtestGenesisBlock.hash, CheckPoint.load(Block.RegtestGenesisBlock.hash)), params)
+
 
   def addFunds(data: Data, key: ExtendedPrivateKey, amount: Satoshi): Data = {
     val tx = Transaction(version = 1, txIn = Nil, txOut = TxOut(amount, data.keyStore.computePublicKeyScript(key.publicKey)) :: Nil, lockTime = 0)
@@ -83,8 +73,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
 
   def addFunds(data: Data, keyamounts: Seq[(ExtendedPrivateKey, Satoshi)]): Data = keyamounts.foldLeft(data)(addFunds)
 
-  test("complete transactions (enough funds)") { f =>
-    import f._
+  test("complete transactions (enough funds)") {
     val state1 = addFunds(state, state.accountKeys.head, 1 btc)
     val (confirmed1, unconfirmed1) = state1.balance
 
@@ -93,6 +82,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     val (state2, tx1, fee1) = state1.completeTransaction(tx, feeRatePerKw, minimumFee, dustLimit, false)
     val Some((_, _, Some(fee))) = state2.computeTransactionDelta(tx1)
     assert(fee == fee1)
+    val actualFeeRate = Transactions.fee2rate(fee, tx1.weight())
 
     val state3 = state2.cancelTransaction(tx1)
     assert(state3 == state1)
@@ -103,29 +93,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     assert(unconfirmed1 - unconfirmed4 >= btc2satoshi(0.5 btc))
   }
 
-  test("complete transactions (enough funds, bech32)", Tag("bech32")) { f =>
-    import f._
-    val state1 = addFunds(state, state.accountKeys.head, 1 btc)
-    val (confirmed1, unconfirmed1) = state1.balance
-
-    val pub = PrivateKey(ByteVector32(ByteVector.fill(32)(1))).publicKey
-    val tx = Transaction(version = 2, txIn = Nil, txOut = TxOut(0.5 btc, Script.pay2pkh(pub)) :: Nil, lockTime = 0)
-    val (state2, tx1, fee1) = state1.completeTransaction(tx, feeRatePerKw, minimumFee, dustLimit, false)
-    val Some((_, _, Some(fee))) = state2.computeTransactionDelta(tx1)
-    assert(fee == fee1)
-
-    val state3 = state2.cancelTransaction(tx1)
-    assert(state3 == state1)
-
-    val state4 = state2.commitTransaction(tx1)
-    val (confirmed4, unconfirmed4) = state4.balance
-    assert(confirmed4 == confirmed1)
-    assert(unconfirmed1 - unconfirmed4 >= btc2satoshi(0.5 btc))
-  }
-
-
-  test("complete transactions (insufficient funds)") { f =>
-    import f._
+  test("complete transactions (insufficient funds)") {
     val state1 = addFunds(state, state.accountKeys.head, 5 btc)
     val tx = Transaction(version = 2, txIn = Nil, txOut = TxOut(6 btc, Script.pay2pkh(state1.accountKeys(0).publicKey)) :: Nil, lockTime = 0)
     val e = intercept[IllegalArgumentException] {
@@ -133,8 +101,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     }
   }
 
-  test("compute the effect of tx") { f =>
-    import f._
+  test("compute the effect of tx") {
     val state1 = addFunds(state, state.accountKeys.head, 1 btc)
     val tx = Transaction(version = 2, txIn = Nil, txOut = TxOut(0.5 btc, Script.pay2pkh(state1.accountKeys(0).publicKey)) :: Nil, lockTime = 0)
     val (state2, tx1, fee1) = state1.completeTransaction(tx, feeRatePerKw, minimumFee, dustLimit, false)
@@ -144,8 +111,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     assert(sent - received - fee == btc2satoshi(0.5 btc))
   }
 
-  test("use actual transaction weight to compute fees") { f =>
-    import f._
+  test("use actual transaction weight to compute fees") {
     val state1 = addFunds(state, (state.accountKeys(0), 5000000 sat) :: (state.accountKeys(1), 6000000 sat) :: (state.accountKeys(2), 4000000 sat) :: Nil)
 
     {
@@ -184,8 +150,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     }
   }
 
-  test("spend all our balance") { f =>
-    import f._
+  test("spend all our balance") {
     val state1 = addFunds(state, state.accountKeys(0), 1 btc)
     val state2 = addFunds(state1, state1.accountKeys(1), 2 btc)
     val state3 = addFunds(state2, state2.changeKeys(0), 0.5 btc)
@@ -199,8 +164,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     assert(tx.txOut.map(_.amount).sum + fee == state3.balance._1 + state3.balance._2)
   }
 
-  test("check that issue #1146 is fixed") { f =>
-    import f._
+  test("check that issue #1146 is fixed") {
     val state3 = addFunds(state, state.changeKeys(0), 0.5 btc)
 
     val pub1 = state.accountKeys(0).publicKey
@@ -217,26 +181,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
     assert(Try(state3.completeTransaction(tx1, 750, 0 sat, dustLimit, true)).isSuccess)
   }
 
-  test("check that issue #1146 is fixed, bech32", Tag("bech32")) { f =>
-    import f._
-    val state3 = addFunds(state, state.changeKeys(0), 0.5 btc)
-
-    val pub1 = state.accountKeys(0).publicKey
-    val pub2 = state.accountKeys(1).publicKey
-    val redeemScript = Scripts.multiSig2of2(pub1, pub2)
-    val pubkeyScript = Script.pay2wsh(redeemScript)
-    val (tx, fee) = state3.spendAll(pubkeyScript, feeRatePerKw = 750)
-    val Some((received, sent, Some(fee1))) = state3.computeTransactionDelta(tx)
-    assert(received === 0.sat)
-    assert(fee == fee1)
-    assert(tx.txOut.map(_.amount).sum + fee == state3.balance._1 + state3.balance._2)
-
-    val tx1 = Transaction(version = 2, txIn = Nil, txOut = TxOut(tx.txOut.map(_.amount).sum, pubkeyScript) :: Nil, lockTime = 0)
-    assert(Try(state3.completeTransaction(tx1, 750, 0 sat, dustLimit, true)).isSuccess)
-  }
-
-  test("fuzzy test") { f =>
-    import f._
+  test("fuzzy test") {
     val random = new Random()
     (0 to 10) foreach { _ =>
       val funds = for (i <- 0 until random.nextInt(10)) yield {
@@ -259,7 +204,7 @@ class ElectrumWalletBasicSpec extends fixture.FunSuite with Logging {
   }
 }
 
-object ElectrumWalletBasicSpec {
+object ElectrumWalletBasicBaseSpec {
   /**
    *
    * @param actualFeeRate actual fee rate
@@ -267,4 +212,12 @@ object ElectrumWalletBasicSpec {
    * @return true if actual fee rate is within 10% of target
    */
   def isFeerateOk(actualFeeRate: Long, targetFeeRate: Long): Boolean = Math.abs(actualFeeRate - targetFeeRate) < 0.1 * (actualFeeRate + targetFeeRate)
+}
+
+class ElectrumWalletBasicBIP49Spec extends ElectrumWalletBasicBaseSpec {
+  override def walletType: ElectrumWallet.WalletType = P2SH_SEGWIT
+}
+
+class ElectrumWalletBasicBIP84Spec extends ElectrumWalletBasicBaseSpec {
+  override def walletType: ElectrumWallet.WalletType = NATIVE_SEGWIT
 }
