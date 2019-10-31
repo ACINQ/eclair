97067720b3bc2fef8741cb270d19cfa9aa234576
anton <admin@btcontract.com>
2019-10-31 18:03:11 +0200

    Fix GUI

--- eclair-node-gui/src/main/scala/fr/acinq/eclair/gui/controllers/OpenChannelController.scala
+++ eclair-node-gui/src/main/scala/fr/acinq/eclair/gui/controllers/OpenChannelController.scala
@@ -90,8 +90,8 @@ class OpenChannelController(val handlers: Handlers, val stage: Stage) extends Lo
             fundingSatError.setText("Capacity must be greater than 0")
           case Success(capacitySat) if capacitySat < 50000.sat =>
             fundingSatError.setText("Capacity is low and the channel may not be able to open")
-          case Success(capacitySat) if capacitySat >= Channel.MAX_FUNDING =>
-            fundingSatError.setText(s"Capacity must be less than ${CoinUtils.formatAmountInUnit(Channel.MAX_FUNDING, FxApp.getUnit, withUnit = true)}")
+          case Success(capacitySat) if capacitySat >= Channel.MAX_FUNDING_SATOSHIS =>
+            fundingSatError.setText(s"Capacity must be less than ${CoinUtils.formatAmountInUnit(Channel.MAX_FUNDING_SATOSHIS, FxApp.getUnit, withUnit = true)}")
           case Success(_) => fundingSatError.setText("")
           case _ => fundingSatError.setText("Capacity is not valid")
         }
@@ -111,8 +111,8 @@ class OpenChannelController(val handlers: Handlers, val stage: Stage) extends Lo
         Try(if (Strings.isNullOrEmpty(feerateField.getText())) None else Some(feerateField.getText().toLong))) match {
         case (Success(capacitySat), _, _) if capacitySat <= 0.sat =>
           fundingSatError.setText("Capacity must be greater than 0")
-        case (Success(capacitySat), _, _) if capacitySat >= Channel.MAX_FUNDING =>
-          fundingSatError.setText(s"Capacity must be less than ${CoinUtils.formatAmountInUnit(Channel.MAX_FUNDING, FxApp.getUnit, withUnit = true)}")
+        case (Success(capacitySat), _, _) if capacitySat >= Channel.MAX_FUNDING_SATOSHIS =>
+          fundingSatError.setText(s"Capacity must be less than ${CoinUtils.formatAmountInUnit(Channel.MAX_FUNDING_SATOSHIS, FxApp.getUnit, withUnit = true)}")
         case (Success(capacitySat), Success(pushMsat), _) if pushMsat > capacitySat.toMilliSatoshi.toLong =>
           pushMsatError.setText("Push must be less or equal to capacity")
         case (Success(_), Success(pushMsat), _) if pushMsat < 0 =>
