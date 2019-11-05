8a7a8029e7c84f71d6113aeea466646a3ff5ebc4
Andrea <andrea.raspitzu@acinq.fr>
2019-11-05 11:50:44 +0100

    Avoid checking parent tx confirmations during watch spent checks

--- eclair-core/src/main/scala/fr/acinq/eclair/blockchain/bitcoind/ZmqWatcher.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/blockchain/bitcoind/ZmqWatcher.scala
@@ -116,26 +116,21 @@ class ZmqWatcher(blockCount: AtomicLong, client: ExtendedBitcoinClient)(implicit
           }
 
         case WatchSpent(_, txid, outputIndex, _, _) =>
-          // first let's see if the parent tx was published or not
-          client.getTxConfirmations(txid.toString()).collect {
-            case Some(_) =>
-              // parent tx was published, we need to make sure this particular output has not been spent
-              client.isTransactionOutputSpendable(txid.toString(), outputIndex, true).collect {
-                case false =>
-                  log.info(s"$txid:$outputIndex has already been spent, looking for the spending tx in the mempool")
-                  client.getMempool().map { mempoolTxs =>
-                    mempoolTxs.filter(tx => tx.txIn.exists(i => i.outPoint.txid == txid && i.outPoint.index == outputIndex)) match {
-                      case Nil =>
-                        log.warning(s"$txid:$outputIndex has already been spent, spending tx not in the mempool, looking in the blockchain...")
-                        client.lookForSpendingTx(None, txid.toString(), outputIndex).map { tx =>
-                          log.warning(s"found the spending tx of $txid:$outputIndex in the blockchain: txid=${tx.txid}")
-                          self ! NewTransaction(tx)
-                        }
-                      case txs =>
-                        log.info(s"found ${txs.size} txs spending $txid:$outputIndex in the mempool: txids=${txs.map(_.txid).mkString(",")}")
-                        txs.foreach(tx => self ! NewTransaction(tx))
+          client.isTransactionOutputSpendable(txid.toString(), outputIndex, true).collect {
+            case false =>
+              log.info(s"$txid:$outputIndex has already been spent, looking for the spending tx in the mempool")
+              client.getMempool().map { mempoolTxs =>
+                mempoolTxs.filter(tx => tx.txIn.exists(i => i.outPoint.txid == txid && i.outPoint.index == outputIndex)) match {
+                  case Nil =>
+                    log.warning(s"$txid:$outputIndex has already been spent, spending tx not in the mempool, looking in the blockchain...")
+                    client.lookForSpendingTx(None, txid.toString(), outputIndex).map { tx =>
+                      log.warning(s"found the spending tx of $txid:$outputIndex in the blockchain: txid=${tx.txid}")
+                      self ! NewTransaction(tx)
                     }
-                  }
+                  case txs =>
+                    log.info(s"found ${txs.size} txs spending $txid:$outputIndex in the mempool: txids=${txs.map(_.txid).mkString(",")}")
+                    txs.foreach(tx => self ! NewTransaction(tx))
+                }
               }
           }
 
