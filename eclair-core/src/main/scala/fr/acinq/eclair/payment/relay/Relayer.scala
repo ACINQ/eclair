1c08704db5077549348ecbc01b6a66cd9f23ebde
Bastien Teinturier <bastuc@hotmail.fr>
2019-11-19 18:05:00 +0100

    Relay Trampoline payments. Recovery after a restart isn't implemented yet. A lot of polish is still needed to send the right errors and emit the correct events.

--- eclair-core/src/main/scala/fr/acinq/eclair/payment/relay/Relayer.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/payment/relay/Relayer.scala
@@ -39,7 +39,13 @@ object Origin {
   case class Local(id: UUID, sender: Option[ActorRef]) extends Origin // we don't persist reference to local actors
   /** Our node forwarded a single incoming HTLC to an outgoing channel. */
   case class Relayed(originChannelId: ByteVector32, originHtlcId: Long, amountIn: MilliSatoshi, amountOut: MilliSatoshi) extends Origin
-  // TODO: @t-bast: add TrampolineRelayed
+  /**
+   * Our node forwarded an incoming HTLC set to a remote outgoing node (potentially producing multiple downstream HTLCs).
+   *
+   * @param origins origin channelIds and htlcIds.
+   * @param sender actor sending the outgoing HTLC (if we haven't restarted and lost the reference).
+   */
+  case class TrampolineRelayed(origins: List[(ByteVector32, Long)], sender: Option[ActorRef]) extends Origin
 }
 // @formatter:on
 
@@ -56,7 +62,7 @@ object Origin {
  * It also receives channel HTLC events (fulfill / failed) and relays those to the appropriate handlers.
  * It also maintains an up-to-date view of local channel balances.
  */
-class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorRef) extends Actor with ActorLogging {
+class Relayer(nodeParams: NodeParams, router: ActorRef, register: ActorRef, paymentHandler: ActorRef) extends Actor with ActorLogging {
 
   import Relayer._
 
@@ -69,6 +75,7 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
 
   private val commandBuffer = context.actorOf(Props(new CommandBuffer(nodeParams, register)))
   private val channelRelayer = context.actorOf(ChannelRelayer.props(nodeParams, self, register, commandBuffer))
+  private val nodeRelayer = context.actorOf(NodeRelayer.props(nodeParams, self, router, commandBuffer, register))
 
   override def receive: Receive = main(Map.empty, new mutable.HashMap[PublicKey, mutable.Set[ShortChannelId]] with mutable.MultiMap[PublicKey, ShortChannelId])
 
@@ -110,9 +117,7 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
             log.warning(s"rejecting htlc #${add.id} paymentHash=${add.paymentHash} from channelId=${add.channelId} to nodeId=${r.innerPayload.outgoingNodeId} reason=trampoline disabled")
             commandBuffer ! CommandBuffer.CommandSend(add.channelId, add.id, CMD_FAIL_HTLC(add.id, Right(RequiredNodeFeatureMissing), commit = true))
           } else {
-            // TODO: @t-bast: relay trampoline payload instead of rejecting.
-            log.warning(s"rejecting htlc #${add.id} paymentHash=${add.paymentHash} from channelId=${add.channelId} to nodeId=${r.innerPayload.outgoingNodeId} reason=trampoline not implemented yet")
-            commandBuffer ! CommandBuffer.CommandSend(add.channelId, add.id, CMD_FAIL_HTLC(add.id, Right(RequiredNodeFeatureMissing), commit = true))
+            nodeRelayer forward r
           }
         case Left(badOnion: BadOnion) =>
           log.warning(s"couldn't parse onion: reason=${badOnion.message}")
@@ -128,12 +133,13 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
     case Status.Failure(addFailed: AddHtlcFailed) =>
       import addFailed.paymentHash
       addFailed.origin match {
-        case Origin.Local(id, None) =>
-          handleLocalPaymentAfterRestart(PaymentFailed(id, paymentHash, Nil))
-        case Origin.Local(_, Some(sender)) =>
-          sender ! Status.Failure(addFailed)
-        case _: Origin.Relayed =>
-          channelRelayer forward Status.Failure(addFailed)
+        case Origin.Local(id, None) => handleLocalPaymentAfterRestart(PaymentFailed(id, paymentHash, Nil))
+        case Origin.Local(_, Some(sender)) => sender ! Status.Failure(addFailed)
+        case _: Origin.Relayed => channelRelayer forward Status.Failure(addFailed)
+        case Origin.TrampolineRelayed(htlcs, None) =>
+          // TODO: @t-bast: reconcile after restart
+          log.error(s"detected pending trampoline payment after restart: not implemented yet, some channels might get closed ($htlcs)...")
+        case Origin.TrampolineRelayed(_, Some(sender)) => sender ! Status.Failure(addFailed)
       }
 
     case ForwardFulfill(fulfill, to, add) =>
@@ -147,6 +153,10 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
           val cmd = CMD_FULFILL_HTLC(originHtlcId, fulfill.paymentPreimage, commit = true)
           commandBuffer ! CommandBuffer.CommandSend(originChannelId, originHtlcId, cmd)
           context.system.eventStream.publish(PaymentRelayed(amountIn, amountOut, add.paymentHash, fromChannelId = originChannelId, toChannelId = fulfill.channelId))
+        case Origin.TrampolineRelayed(htlcs, None) =>
+          // TODO: @t-bast: reconcile after restart
+          log.error(s"detected pending trampoline payment after restart: not implemented yet, some channels might get closed ($htlcs)...")
+        case Origin.TrampolineRelayed(_, Some(sender)) => sender ! fulfill
       }
 
     case ForwardFail(fail, to, add) =>
@@ -158,6 +168,10 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
         case Origin.Relayed(originChannelId, originHtlcId, _, _) =>
           val cmd = CMD_FAIL_HTLC(originHtlcId, Left(fail.reason), commit = true)
           commandBuffer ! CommandBuffer.CommandSend(originChannelId, originHtlcId, cmd)
+        case Origin.TrampolineRelayed(htlcs, None) =>
+          // TODO: @t-bast: reconcile after restart
+          log.error(s"detected pending trampoline payment after restart: not implemented yet, some channels might get closed ($htlcs)...")
+        case Origin.TrampolineRelayed(_, Some(sender)) => sender ! fail
       }
 
     case ForwardFailMalformed(fail, to, add) =>
@@ -169,6 +183,10 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
         case Origin.Relayed(originChannelId, originHtlcId, _, _) =>
           val cmd = CMD_FAIL_MALFORMED_HTLC(originHtlcId, fail.onionHash, fail.failureCode, commit = true)
           commandBuffer ! CommandBuffer.CommandSend(originChannelId, originHtlcId, cmd)
+        case Origin.TrampolineRelayed(htlcs, None) =>
+          // TODO: @t-bast: reconcile after restart
+          log.error(s"detected pending trampoline payment after restart: not implemented yet, some channels might get closed ($htlcs)...")
+        case Origin.TrampolineRelayed(_, Some(sender)) => sender ! fail
       }
 
     case ack: CommandBuffer.CommandAck => commandBuffer forward ack
@@ -180,6 +198,8 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
    * It may happen that we sent a payment and then re-started before the payment completed.
    * When we receive the HTLC fulfill/fail associated to that payment, the payment FSM that generated them doesn't exist
    * anymore so we need to reconcile the database.
+   *
+   * TODO: @t-bast: this should move to a dedicated "PostRestartCleanup" actor that also handles broken trampoline MPP relays.
    */
   def handleLocalPaymentAfterRestart(paymentResult: PaymentEvent): Unit = paymentResult match {
     case e: PaymentFailed =>
@@ -211,7 +231,7 @@ class Relayer(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorR
 
 object Relayer extends Logging {
 
-  def props(nodeParams: NodeParams, register: ActorRef, paymentHandler: ActorRef) = Props(classOf[Relayer], nodeParams, register, paymentHandler)
+  def props(nodeParams: NodeParams, router: ActorRef, register: ActorRef, paymentHandler: ActorRef) = Props(classOf[Relayer], nodeParams, router, register, paymentHandler)
 
   type ChannelUpdates = Map[ShortChannelId, OutgoingChannel]
   type NodeChannels = mutable.HashMap[PublicKey, mutable.Set[ShortChannelId]] with mutable.MultiMap[PublicKey, ShortChannelId]
