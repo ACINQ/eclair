5821eb812ce2167b2d1d5c12111ec20af8af444f
anton <admin@btcontract.com>
2019-11-23 14:43:55 +0200

    Remove ERR_HOSTED_UPDATE_CLTV_TOO_LOW exception type also lower default hosted ChannelUpdate CLTV to 144 blocks

--- eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelExceptions.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelExceptions.scala
@@ -97,7 +97,6 @@ object ChannelErrorCodes {
   final val ERR_HOSTED_WRONG_BLOCKDAY = ByteVector.fromValidHex("0001")
   final val ERR_HOSTED_WRONG_LOCAL_SIG = ByteVector.fromValidHex("0002")
   final val ERR_HOSTED_WRONG_REMOTE_SIG = ByteVector.fromValidHex("0003")
-  final val ERR_HOSTED_UPDATE_CLTV_TOO_LOW = ByteVector.fromValidHex("0004")
   final val ERR_HOSTED_TOO_MANY_STATE_UPDATES = ByteVector.fromValidHex("0005")
   final val ERR_HOSTED_TIMED_OUT_OUTGOING_HTLC = ByteVector.fromValidHex("0006")
   final val ERR_HOSTED_IN_FLIGHT_HTLC_WHILE_RESTORING = ByteVector.fromValidHex("0007")
@@ -107,7 +106,6 @@ object ChannelErrorCodes {
     ERR_HOSTED_WRONG_BLOCKDAY,
     ERR_HOSTED_WRONG_LOCAL_SIG,
     ERR_HOSTED_WRONG_REMOTE_SIG,
-    ERR_HOSTED_UPDATE_CLTV_TOO_LOW,
     ERR_HOSTED_TOO_MANY_STATE_UPDATES,
     ERR_HOSTED_TIMED_OUT_OUTGOING_HTLC,
     ERR_HOSTED_IN_FLIGHT_HTLC_WHILE_RESTORING,
