c668fabccdf239f7164b62a1b45c782cefe4aeb7
anton <admin@btcontract.com>
2019-11-27 16:19:16 +0200

    Minor refactoring

--- eclair-core/src/main/scala/fr/acinq/eclair/db/AkkaBackupHandler.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/db/AkkaBackupHandler.scala
@@ -15,7 +15,7 @@ import scala.concurrent.ExecutionContext
 
 class AkkaBackupHandler(nodeParams: NodeParams, incomingNodeIds: Set[PublicKey], outgoingNodeAddresses: Set[String], localBackupFile: File, remoteBackupDir: File, throttle: Int)(implicit ec: ExecutionContext = ExecutionContext.Implicits.global) extends Actor with ActorLogging {
 
-  context.system.scheduler.schedule(throttle.seconds, throttle.seconds, self, AkkaBackupHandler.SEND_AKKA_BACKUP)
+  context.system.scheduler.schedule(throttle.seconds, throttle.seconds, self, AkkaBackupHandler.SEND_AKKA_BACKUP_TO_REMOTE_NODE)
 
   // we listen to ChannelPersisted events, which will schedule a backup
   context.system.eventStream.subscribe(self, classOf[ChannelPersisted])
@@ -26,7 +26,7 @@ class AkkaBackupHandler(nodeParams: NodeParams, incomingNodeIds: Set[PublicKey],
 
   override def receive: Receive = main(None)
 
-  def main(abOpt: Option[AkkaBackup] = None): Receive = {
+  def main(akkaBackupOpt: Option[AkkaBackup] = None): Receive = {
     case ab: AkkaBackup if incomingNodeIds.contains(ab.remoteNodeId) && ab.isValidSig =>
       val backupFile = new File(remoteBackupDir.getAbsolutePath, ab.remoteNodeId.toString.concat(".eclair.sqlite.bak"))
       Files.write(ab.data.toArray, backupFile)
@@ -41,14 +41,16 @@ class AkkaBackupHandler(nodeParams: NodeParams, incomingNodeIds: Set[PublicKey],
       val ab = AkkaBackup(backup, nodeParams.nodeId, localSig)
       context become main(Some(ab))
 
-    case AkkaBackupHandler.SEND_AKKA_BACKUP =>
-      for {
-        ab <- abOpt
-        addressAndPort <- outgoingNodeAddresses
-        path = s"akka.tcp://${context.system.name}@$addressAndPort/user/${AkkaBackupHandler.actorName}"
-      } context.actorSelection(path) ! ab
+    case AkkaBackupHandler.SEND_AKKA_BACKUP_TO_REMOTE_NODE =>
+      akkaBackupOpt.foreach(remoteSend)
       context become main(None)
   }
+
+  def remoteSend(message: Any): Unit =
+    for {
+      addressAndPort <- outgoingNodeAddresses
+      path = s"akka.tcp://${context.system.name}@$addressAndPort/user/${AkkaBackupHandler.actorName}" // TODO: system name will be eclair-node or eclair-node-gui, fix that
+    } context.actorSelection(path) ! message
 }
 
 case class AkkaBackup(data: ByteVector, remoteNodeId: PublicKey, signature: ByteVector64) {
@@ -58,7 +60,7 @@ case class AkkaBackup(data: ByteVector, remoteNodeId: PublicKey, signature: Byte
 object AkkaBackupHandler {
   final val actorName = "akkaBackupHandler"
 
-  case object SEND_AKKA_BACKUP
+  case object SEND_AKKA_BACKUP_TO_REMOTE_NODE
 
   def props(nodeParams: NodeParams, incomingNodeIds: Set[PublicKey], outgoingNodeAddresses: Set[String], localBackupFile: File, remoteBackupDir: File, throttle: Int) =
     Props(new AkkaBackupHandler(nodeParams, incomingNodeIds, outgoingNodeAddresses, localBackupFile, remoteBackupDir, throttle))
