31a30d20b46699eaf617d9f5321c8e8a55daacb4
Bastien Teinturier <bastuc@hotmail.fr>
2019-12-16 13:46:26 +0100

    Add TLV stream to Init message

--- eclair-core/src/main/scala/fr/acinq/eclair/wire/TlvTypes.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/wire/TlvTypes.scala
@@ -22,41 +22,40 @@ import scodec.bits.ByteVector
 import scala.reflect.ClassTag
 
 /**
-  * Created by t-bast on 20/06/2019.
-  */
+ * Created by t-bast on 20/06/2019.
+ */
 
 trait Tlv
 
 /**
-  * Generic tlv type we fallback to if we don't understand the incoming tlv.
-  *
-  * @param tag   tlv tag.
-  * @param value tlv value (length is implicit, and encoded as a varint).
-  */
+ * Generic tlv type we fallback to if we don't understand the incoming tlv.
+ *
+ * @param tag   tlv tag.
+ * @param value tlv value (length is implicit, and encoded as a varint).
+ */
 case class GenericTlv(tag: UInt64, value: ByteVector) extends Tlv
 
 /**
-  * A tlv stream is a collection of tlv records.
-  * A tlv stream is constrained to a specific tlv namespace that dictates how to parse the tlv records.
-  * That namespace is provided by a trait extending the top-level tlv trait.
-  *
-  * @param records known tlv records.
-  * @param unknown unknown tlv records.
-  * @tparam T the stream namespace is a trait extending the top-level tlv trait.
-  */
+ * A tlv stream is a collection of tlv records.
+ * A tlv stream is constrained to a specific tlv namespace that dictates how to parse the tlv records.
+ * That namespace is provided by a trait extending the top-level tlv trait.
+ *
+ * @param records known tlv records.
+ * @param unknown unknown tlv records.
+ * @tparam T the stream namespace is a trait extending the top-level tlv trait.
+ */
 case class TlvStream[T <: Tlv](records: Traversable[T], unknown: Traversable[GenericTlv] = Nil) {
   /**
-    *
-    * @tparam R input type parameter, must be a subtype of the main TLV type
-    * @return the TLV record of type that matches the input type parameter if any (there can be at most one, since BOLTs specify
-    *         that TLV records are supposed to be unique)
-    */
+   *
+   * @tparam R input type parameter, must be a subtype of the main TLV type
+   * @return the TLV record of type that matches the input type parameter if any (there can be at most one, since BOLTs specify
+   *         that TLV records are supposed to be unique)
+   */
   def get[R <: T : ClassTag]: Option[R] = records.collectFirst { case r: R => r }
 }
 
 object TlvStream {
-  def empty[T <: Tlv] = TlvStream[T](Nil, Nil)
+  def empty[T <: Tlv]: TlvStream[T] = TlvStream[T](Nil, Nil)
 
   def apply[T <: Tlv](records: T*): TlvStream[T] = TlvStream(records, Nil)
-
 }
