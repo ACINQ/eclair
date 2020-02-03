f35c2e7afa3dcf1854baf7c1ec0932657171cf77
pm47 <pm.padiou@gmail.com>
2020-02-03 11:22:21 +0100

    rework `AvailableBalanceChanged` events
    Instead of emitting this event when we send a signature, we emit it when
    our `availableBalanceForSend` actually changes. This happens:
    - when we send a new `update_add` or `update_fee`;
    - when we receive a `commit_sig`, which may acknowledge one or several
    `update_fulfill`, `update_fail_*`, or `update_fee` sent by our peer.
    
    We choose to only emit this event in `NORMAL` state, because its goal is
    to give information about what payments we can make, which can only
    happen in that state.
    
    NB: other events `ChannelSignatureSent` and `ChannelSignatureReceived` give
    a different type of information, and are sent in all states where
    signatures are exchanges, not only in `NORMAL`.
    
    The field `localBalance` has been moved to a function and is ambiguous
    (there are multiple ways of computing our balance. We should probably
    consider removing it altogether, it is only used for audit purposes and
    arguably not very useful there.

--- eclair-core/src/test/scala/fr/acinq/eclair/channel/states/e/NormalStateSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/channel/states/e/NormalStateSpec.scala
@@ -69,10 +69,14 @@ class NormalStateSpec extends TestkitBaseClass with StateTestsHelperMethods {
     import f._
     val initialState = alice.stateData.asInstanceOf[DATA_NORMAL]
     val sender = TestProbe()
+    val listener = TestProbe()
+    system.eventStream.subscribe(listener.ref, classOf[AvailableBalanceChanged])
     val h = randomBytes32
     val add = CMD_ADD_HTLC(50000000 msat, h, CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), TestConstants.emptyOnionPacket, Upstream.Local(UUID.randomUUID()))
     sender.send(alice, add)
     sender.expectMsg("ok")
+    val e = listener.expectMsgType[AvailableBalanceChanged]
+    assert(e.commitments.availableBalanceForSend < initialState.commitments.availableBalanceForSend)
     val htlc = alice2bob.expectMsgType[UpdateAddHtlc]
     assert(htlc.id == 0 && htlc.paymentHash == h)
     awaitCond(alice.stateData == initialState.copy(
