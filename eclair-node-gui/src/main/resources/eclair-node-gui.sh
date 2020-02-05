fbaa80497835c677a2212a95a90c7bbc5a93e793
Andrea <andrea.raspitzu@acinq.fr>
2020-02-05 16:43:43 +0100

    Replace maven-capsule-plugin with maven-assembly-plugin in eclair-node and eclair-node-gui:  The new packaging produces a zip file containing a launcher script and  all the artifacts and dependencies in lib/.

---
+++ eclair-node-gui/src/main/resources/eclair-node-gui.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+echo -n "Launching eclair"
+
+java -cp lib/:lib/eclair-node-gui_2.11-0.3.4-SNAPSHOT.jar fr.acinq.eclair.JavafxBoot
---
+++ eclair-node/src/main/resources/eclair-node.sh
@@ -0,0 +1,5 @@
+!# /bin/sh
+
+echo -n "Launching eclair"
+
+java -cp lib/:lib/eclair-node_2.11-0.3.4-SNAPSHOT.jar fr.acinq.eclair.Boot
