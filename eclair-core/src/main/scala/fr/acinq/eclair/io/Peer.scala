0c226048d65102a8d6e7de85dedde3458389b1a6
pm47 <pm.padiou@gmail.com>
2020-05-13 15:56:04 +0200

    atomically switch to new connection
    When a new connection happens while already connected, the `Peer` will
    switch to the new connection.
    
    In the current implementation, upon receiving a `ConnectionReady`, the
    `Peer` will kill the current connection, then sends back the
    `ConnectionReady` message to itself. In the meantime, the
    `PeerConnection` will happily forward any incoming messages to the `Peer`.
    
    This opens up to a race in the `Peer`'s mailbox between the
    `ConnectionReady` message, and any incoming messages from the new
    connection. If the latter win, they will get dropped because the `Peer`
    is in state `DISCONNECTED`. Typically those are `ChannelReestablish`
    messages and channels will get stuck in state `SYNCING`.
    
    This PR make the `Peer` atomically switch to the new connection, without
    going back to the `DISCONNECTED` state. As a result, we now have a
    `CONNECTED`->`CONNECTED` transition.

--- eclair-core/src/main/scala/fr/acinq/eclair/io/Peer.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/io/Peer.scala
@@ -66,22 +66,8 @@ class Peer(val nodeParams: NodeParams, remoteNodeId: PublicKey, watcher: ActorRe
       reconnectionTask forward p
       stay
 
-    case Event(PeerConnection.ConnectionReady(peerConnection, remoteNodeId1, address, outgoing, localInit, remoteInit), d: DisconnectedData) =>
-      require(remoteNodeId == remoteNodeId1, s"invalid nodeid: $remoteNodeId != $remoteNodeId1")
-      log.debug("got authenticated connection to address {}:{}", address.getHostString, address.getPort)
-
-      context watch peerConnection
-
-      if (outgoing) {
-        // we store the node address upon successful outgoing connection, so we can reconnect later
-        // any previous address is overwritten
-        NodeAddress.fromParts(address.getHostString, address.getPort).map(nodeAddress => nodeParams.db.peers.addOrUpdatePeer(remoteNodeId, nodeAddress))
-      }
-
-      // let's bring existing/requested channels online
-      d.channels.values.toSet[ActorRef].foreach(_ ! INPUT_RECONNECTED(peerConnection, localInit, remoteInit)) // we deduplicate with toSet because there might be two entries per channel (tmp id and final id)
-
-      goto(CONNECTED) using ConnectedData(address, peerConnection, localInit, remoteInit, d.channels.map { case (k: ChannelId, v) => (k, v) })
+    case Event(connectionReady: PeerConnection.ConnectionReady, d: DisconnectedData) =>
+      gotoConnected(connectionReady, d.channels.map { case (k: ChannelId, v) => (k, v) })
 
     case Event(Terminated(actor), d: DisconnectedData) if d.channels.exists(_._2 == actor) =>
       val h = d.channels.filter(_._2 == actor).keys
@@ -210,8 +196,7 @@ class Peer(val nodeParams: NodeParams, remoteNodeId: PublicKey, watcher: ActorRe
         context unwatch d.peerConnection
         d.peerConnection ! PoisonPill
         d.channels.values.toSet[ActorRef].foreach(_ ! INPUT_DISCONNECTED) // we deduplicate with toSet because there might be two entries per channel (tmp id and final id)
-        self forward connectionReady // we preserve the origin
-        goto(DISCONNECTED) using DisconnectedData(d.channels.collect { case (k: FinalChannelId, v) => (k, v) })
+        gotoConnected(connectionReady, d.channels)
 
       case Event(unhandledMsg: LightningMessage, _) =>
         log.warning("ignoring message {}", unhandledMsg)
@@ -241,8 +226,10 @@ class Peer(val nodeParams: NodeParams, remoteNodeId: PublicKey, watcher: ActorRe
   }
 
   onTransition {
-    case _ -> CONNECTED =>
+    case DISCONNECTED -> CONNECTED =>
       Metrics.PeersConnected.withoutTags().increment()
+      context.system.eventStream.publish(PeerConnected(self, remoteNodeId))
+    case CONNECTED -> CONNECTED => // connection switch
       context.system.eventStream.publish(PeerConnected(self, remoteNodeId))
     case CONNECTED -> DISCONNECTED =>
       Metrics.PeersConnected.withoutTags().decrement()
@@ -254,6 +241,24 @@ class Peer(val nodeParams: NodeParams, remoteNodeId: PublicKey, watcher: ActorRe
       // the transition handler won't be fired if we go directly from CONNECTED to closed
       Metrics.PeersConnected.withoutTags().decrement()
       context.system.eventStream.publish(PeerDisconnected(self, remoteNodeId))
+  }
+
+  def gotoConnected(connectionReady: PeerConnection.ConnectionReady, channels: Map[ChannelId, ActorRef]): State = {
+    require(remoteNodeId == connectionReady.remoteNodeId, s"invalid nodeid: $remoteNodeId != ${connectionReady.remoteNodeId}")
+    log.debug("got authenticated connection to address {}:{}", connectionReady.address.getHostString, connectionReady.address.getPort)
+
+    context watch connectionReady.peerConnection
+
+    if (connectionReady.outgoing) {
+      // we store the node address upon successful outgoing connection, so we can reconnect later
+      // any previous address is overwritten
+      NodeAddress.fromParts(connectionReady.address.getHostString, connectionReady.address.getPort).map(nodeAddress => nodeParams.db.peers.addOrUpdatePeer(remoteNodeId, nodeAddress))
+    }
+
+    // let's bring existing/requested channels online
+    channels.values.toSet[ActorRef].foreach(_ ! INPUT_RECONNECTED(connectionReady.peerConnection, connectionReady.localInit, connectionReady.remoteInit)) // we deduplicate with toSet because there might be two entries per channel (tmp id and final id)
+
+    goto(CONNECTED) using ConnectedData(connectionReady.address, connectionReady.peerConnection, connectionReady.localInit, connectionReady.remoteInit, channels)
   }
 
   /**
