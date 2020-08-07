01f924ae86dae30b2e626c5c445600049fbf81fc
Donovan <16935834+dkrm0@users.noreply.github.com>
2020-08-07 16:33:20 +0200

    Add API commands to sign & verify arbitrary messages (#1499)
    It can be useful to sign arbitrary messages with the key associated with our node_id (to prove ownership of a node).
    
    Adds 2 new API commands:
      eclair-cli signmessage --msg=${message}
      eclair-cli verifymessage --msg=${message} --sig=${signature}

--- eclair-core/src/main/scala/fr/acinq/eclair/crypto/KeyManager.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/crypto/KeyManager.scala
@@ -110,6 +110,19 @@ trait KeyManager {
    *         private key, bitcoinSig is the signature of the channel announcement with our funding private key
    */
   def signChannelAnnouncement(fundingKeyPath: DeterministicWallet.KeyPath, chainHash: ByteVector32, shortChannelId: ShortChannelId, remoteNodeId: PublicKey, remoteFundingKey: PublicKey, features: Features): (ByteVector64, ByteVector64)
+
+  /**
+   * Sign a digest, primarily used to prove ownership of the current node
+   *
+   * When recovering a public key from an ECDSA signature for secp256k1, there are 4 possible matching curve points
+   * that can be found. The recoveryId identifies which of these points is the correct.
+   *
+   * @param digest     SHA256 digest
+   * @param privateKey private key to sign with, default the one from the current node
+   * @return a (signature, recoveryId) pair. signature is a signature of the digest parameter generated with the
+   *         private key given in parameter. recoveryId is the corresponding recoveryId of the signature
+   */
+  def signDigest(digest: ByteVector32, privateKey: PrivateKey = nodeKey.privateKey): (ByteVector64, Int)
 }
 
 object KeyManager {
