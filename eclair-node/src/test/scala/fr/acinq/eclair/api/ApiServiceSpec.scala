42481c66e6752a2aff274c7e71960d4e99308ffd
Bastien Teinturier <31281497+t-bast@users.noreply.github.com>
2020-09-28 09:51:42 +0200

    Add some channel events to websocket (#1536)
    Send basic channel events to websockets listeners:
    
    * Channel open initiated
    * Channel state change
    * Channel closed
    
    We only send basic, high-level data about these events.
    If the listener is interested in details, it should call the `channelInfo`
    API to get all of the channel's data.
    
    Fixes #1509

--- eclair-node/src/test/scala/fr/acinq/eclair/api/ApiServiceSpec.scala
+++ eclair-node/src/test/scala/fr/acinq/eclair/api/ApiServiceSpec.scala
@@ -33,7 +33,9 @@ import fr.acinq.eclair.ApiTypes.ChannelIdentifier
 import fr.acinq.eclair.FeatureSupport.{Mandatory, Optional}
 import fr.acinq.eclair.Features.{ChannelRangeQueriesExtended, OptionDataLossProtect}
 import fr.acinq.eclair._
-import fr.acinq.eclair.channel.{CMD_CLOSE, ChannelOpenResponse, CommandResponse, RES_SUCCESS}
+import fr.acinq.eclair.blockchain.fee.FeeratePerKw
+import fr.acinq.eclair.channel.Helpers.Closing
+import fr.acinq.eclair.channel._
 import fr.acinq.eclair.db._
 import fr.acinq.eclair.io.NodeURI
 import fr.acinq.eclair.io.Peer.PeerInfo
@@ -530,6 +532,24 @@ class ApiServiceSpec extends AnyFunSuite with ScalatestRouteTest with IdiomaticM
         assert(serialization.write(pset) === expectedSerializedPset)
         system.eventStream.publish(pset)
         wsClient.expectMessage(expectedSerializedPset)
+
+        val chcr = ChannelCreated(system.deadLetters, system.deadLetters, bobNodeId, isFunder = true, ByteVector32.One, FeeratePerKw(25 sat), Some(FeeratePerKw(20 sat)))
+        val expectedSerializedChcr = """{"type":"channel-opened","remoteNodeId":"039dc0e0b1d25905e44fdf6f8e89755a5e219685840d0bc1d28d3308f9628a3585","isFunder":true,"temporaryChannelId":"0100000000000000000000000000000000000000000000000000000000000000","initialFeeratePerKw":25,"fundingTxFeeratePerKw":20}"""
+        assert(serialization.write(chcr) === expectedSerializedChcr)
+        system.eventStream.publish(chcr)
+        wsClient.expectMessage(expectedSerializedChcr)
+
+        val chsc = ChannelStateChanged(system.deadLetters, system.deadLetters, bobNodeId, OFFLINE, NORMAL, null)
+        val expectedSerializedChsc = """{"type":"channel-state-changed","remoteNodeId":"039dc0e0b1d25905e44fdf6f8e89755a5e219685840d0bc1d28d3308f9628a3585","previousState":"OFFLINE","currentState":"NORMAL"}"""
+        assert(serialization.write(chsc) === expectedSerializedChsc)
+        system.eventStream.publish(chsc)
+        wsClient.expectMessage(expectedSerializedChsc)
+
+        val chcl = ChannelClosed(system.deadLetters, ByteVector32.One, Closing.NextRemoteClose(null, null), null)
+        val expectedSerializedChcl = """{"type":"channel-closed","channelId":"0100000000000000000000000000000000000000000000000000000000000000","closingType":"NextRemoteClose"}"""
+        assert(serialization.write(chcl) === expectedSerializedChcl)
+        system.eventStream.publish(chcl)
+        wsClient.expectMessage(expectedSerializedChcl)
       }
   }
 
