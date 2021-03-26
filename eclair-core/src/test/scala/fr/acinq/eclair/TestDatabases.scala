ed8a93d85c7c8c1e40602f03433fc827da739ae4
pm47 <pm.padiou@gmail.com>
2021-03-26 11:49:58 +0100

    fixup! move test dabases to their own file

--- eclair-core/src/test/scala/fr/acinq/eclair/TestDatabases.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/TestDatabases.scala
@@ -40,7 +40,7 @@ object TestDatabases {
   case class TestSqliteDatabases() extends TestDatabases {
     // @formatter:off
   override val connection: Connection = sqliteInMemory()
-  override lazy val db = Databases.sqliteDatabaseByConnections(connection, connection, connection)
+  override lazy val db: Databases = Databases.sqliteDatabaseByConnections(connection, connection, connection)
   override def getVersion(statement: Statement, db_name: String, currentVersion: Int): Int = SqliteUtils.getVersion(statement, db_name, currentVersion)
   override def close(): Unit = ()
   // @formatter:on
@@ -63,7 +63,7 @@ object TestDatabases {
 
     // @formatter:off
   override val connection: Connection = pg.getPostgresDatabase.getConnection
-  override lazy val db = Databases.postgresJDBC(hikariConfig, UUID.randomUUID(), lock, jdbcUrlFile_opt = Some(jdbcUrlFile))
+  override lazy val db: Databases = Databases.postgresJDBC(hikariConfig, UUID.randomUUID(), lock, jdbcUrlFile_opt = Some(jdbcUrlFile))
   override def getVersion(statement: Statement, db_name: String, currentVersion: Int): Int = PgUtils.getVersion(statement, db_name, currentVersion)
   override def close(): Unit = pg.close()
   // @formatter:on
