632d40c27030a848419819e824f9f9a6249693a1
Anton Kumaigorodski <akumaigorodski@gmail.com>
2021-09-08 15:40:40 +0300

    Add `AbstractChannelRestored` event trait (#1927)
    This lets plugins define variants of the `ChannelRestored` when
    using non-standard channel types, while ensuring these channels
    are correctly integrated in the list of local channels for payment
    relaying.

--- eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelEvents.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelEvents.scala
@@ -32,7 +32,15 @@ trait ChannelEvent
 
 case class ChannelCreated(channel: ActorRef, peer: ActorRef, remoteNodeId: PublicKey, isFunder: Boolean, temporaryChannelId: ByteVector32, initialFeeratePerKw: FeeratePerKw, fundingTxFeeratePerKw: Option[FeeratePerKw]) extends ChannelEvent
 
-case class ChannelRestored(channel: ActorRef, channelId: ByteVector32, peer: ActorRef, remoteNodeId: PublicKey, data: HasCommitments) extends ChannelEvent
+// This trait can be used by non-standard channels to inject themselves into Register actor and thus make them usable for routing
+trait AbstractChannelRestored extends ChannelEvent {
+  val channel: ActorRef
+  val channelId: ByteVector32
+  val peer: ActorRef
+  val remoteNodeId: PublicKey
+}
+
+case class ChannelRestored(channel: ActorRef, channelId: ByteVector32, peer: ActorRef, remoteNodeId: PublicKey, data: HasCommitments) extends AbstractChannelRestored
 
 case class ChannelIdAssigned(channel: ActorRef, remoteNodeId: PublicKey, temporaryChannelId: ByteVector32, channelId: ByteVector32) extends ChannelEvent
 
