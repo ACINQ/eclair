48e7b0228d71a34a3891971b18c0ba67fdfa5918
anton <akumaigorodski@gmail.com>
2021-09-08 14:08:26 +0300

    Improve test

--- eclair-core/src/test/scala/fr/acinq/eclair/channel/RegisterSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/channel/RegisterSpec.scala
@@ -2,29 +2,23 @@ package fr.acinq.eclair.channel
 
 import fr.acinq.eclair._
 
-import scala.concurrent.duration._
-import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.{ActorRef, Props}
 import akka.testkit.TestProbe
 import fr.acinq.bitcoin.ByteVector32
 import fr.acinq.bitcoin.Crypto.PublicKey
-import org.scalatest.funsuite.AnyFunSuite
-import akka.pattern.ask
-import akka.util.Timeout
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatest.ParallelTestExecution
 
-import scala.concurrent.Await
-
-class RegisterSpec extends AnyFunSuite {
+class RegisterSpec extends TestKitBaseClass with AnyFunSuiteLike with ParallelTestExecution {
 
   case class CustomChannelRestored(channel: ActorRef, channelId: ByteVector32, peer: ActorRef, remoteNodeId: PublicKey) extends AbstractChannelRestored
 
   test("register processes custom restored events") {
-    implicit val timeout: Timeout = Timeout(3.seconds)
-    implicit val system: ActorSystem = ActorSystem("test")
-
+    val sender = TestProbe()
     val registerRef = system.actorOf(Props(new Register))
     val customRestoredEvent = CustomChannelRestored(TestProbe().ref, randomBytes32(), TestProbe().ref, randomKey().publicKey)
     registerRef ! customRestoredEvent
-
-    assert(Await.result(registerRef ? Symbol("channels"), 3.seconds).asInstanceOf[Map[ByteVector32, ActorRef]].keys.head == customRestoredEvent.channelId)
+    sender.send(registerRef, Symbol("channels"))
+    sender.expectMsgType[Map[ByteVector32, ActorRef]] == Map(customRestoredEvent.channelId -> customRestoredEvent.channel)
   }
 }
