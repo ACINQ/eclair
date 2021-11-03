bcf0c7abc86c60257b4bc79eb8f3411303afa909
pm47 <pm.padiou@gmail.com>
2021-11-03 10:31:17 +0100

    define json serializers for default cmd responses

--- eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelData.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/channel/ChannelData.scala
@@ -229,13 +229,12 @@ final case class RES_ADD_SETTLED[+O <: Origin, +R <: HtlcResult](origin: O, htlc
 final case class RES_GETSTATE[+S <: ChannelState](state: S) extends CommandSuccess[CMD_GETSTATE]
 final case class RES_GETSTATEDATA[+D <: ChannelData](data: D) extends CommandSuccess[CMD_GETSTATEDATA]
 final case class RES_GETINFO(nodeId: PublicKey, channelId: ByteVector32, state: ChannelState, data: ChannelData) extends CommandSuccess[CMD_GETINFO]
-final case class RES_CLOSE(channelId: ByteVector32) extends CommandSuccess[CMD_CLOSE]
 
 /**
  * Those are not response to [[Command]], but to [[fr.acinq.eclair.io.Peer.OpenChannel]]
  *
  * If actor A sends a [[fr.acinq.eclair.io.Peer.OpenChannel]] and actor B sends a [[CMD_CLOSE]], then A will receive a
- * [[ChannelOpenResponse.ChannelClosed]] whereas B will receive a [[RES_CLOSE]]
+ * [[ChannelOpenResponse.ChannelClosed]] whereas B will receive a [[RES_SUCCESS]]
  */
 sealed trait ChannelOpenResponse
 object ChannelOpenResponse {
