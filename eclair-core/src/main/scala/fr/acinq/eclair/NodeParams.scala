c370abe5c3c0333c1e650383da736c5f0721b7be
Thomas HUET <81159533+thomash-acinq@users.noreply.github.com>
2021-12-16 10:20:07 +0100

    Rate limit onion messages (#2090)
    Rate limiting is applied per peer connection in both directions

--- eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/NodeParams.scala
@@ -459,7 +459,8 @@ object NodeParams extends Logging {
         pingTimeout = FiniteDuration(config.getDuration("peer-connection.ping-timeout").getSeconds, TimeUnit.SECONDS),
         pingDisconnect = config.getBoolean("peer-connection.ping-disconnect"),
         maxRebroadcastDelay = FiniteDuration(config.getDuration("router.broadcast-interval").getSeconds, TimeUnit.SECONDS), // it makes sense to not delay rebroadcast by more than the rebroadcast period
-        killIdleDelay = FiniteDuration(config.getDuration("onion-messages.kill-transient-connection-after").getSeconds, TimeUnit.SECONDS)
+        killIdleDelay = FiniteDuration(config.getDuration("onion-messages.kill-transient-connection-after").getSeconds, TimeUnit.SECONDS),
+        maxOnionMessagesPerSecond = config.getDouble("onion-messages.max-per-peer-per-second")
       ),
       routerConf = RouterConf(
         channelExcludeDuration = FiniteDuration(config.getDuration("router.channel-exclude-duration").getSeconds, TimeUnit.SECONDS),
