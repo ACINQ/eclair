78c24eecb86afb8d5cf571caa28db948fbe51e68
Richard Myers <remyers@yakshaver.org>
2022-02-17 10:00:49 +0100

    Simplify to a single synchronous state

--- eclair-core/src/main/scala/fr/acinq/eclair/payment/receive/InvoicePurger.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/payment/receive/InvoicePurger.scala
@@ -16,16 +16,14 @@
 
 package fr.acinq.eclair.payment.receive
 
-import akka.Done
 import akka.actor.typed.Behavior
 import akka.actor.typed.eventstream.EventStream
 import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
 import fr.acinq.eclair.db.IncomingPaymentsDb
-import fr.acinq.eclair.payment.receive.InvoicePurger.{Command, PurgeCompleted, PurgeResult, TickPurge}
+import fr.acinq.eclair.payment.receive.InvoicePurger.{Command, PurgeCompleted, TickPurge}
 import fr.acinq.eclair.{TimestampMilli, TimestampMilliLong}
 
 import scala.concurrent.duration.FiniteDuration
-import scala.util.{Failure, Success, Try}
 
 /**
  * This actor will purge expired invoices from the database it was initialized with at a scheduled interval.
@@ -45,8 +43,6 @@ object InvoicePurger {
 
   sealed trait PurgeEvent
 
-  private case class PurgeResult(result: Try[Done]) extends Command
-
   // this notification is sent when we have completed our invoice purge process
   case object PurgeCompleted extends PurgeEvent
 
@@ -57,35 +53,17 @@ class InvoicePurger private(paymentsDb: IncomingPaymentsDb, context: ActorContex
 
   // purge at each tick unless currently purging
   def waiting(): Behavior[Command] =
-    Behaviors.receiveMessagePartial {
+    Behaviors.receiveMessage {
       case TickPurge =>
-        doPurge()
-        purging()
-    }
+        val now = TimestampMilli.now()
+        val expiredPayments = paymentsDb.listExpiredIncomingPayments(0 unixms, now)
+        // purge expired payments
+        expiredPayments.foreach(p => paymentsDb.removeIncomingPayment(p.invoice.paymentHash))
 
-  // ignore ticks and wait for for purge to complete
-  def purging(): Behavior[Command] =
-    Behaviors.receiveMessagePartial {
-      case PurgeResult(res) =>
-        res match {
-          case Success(Done) => context.log.debug("invoice purge succeeded")
-          case Failure(cause) => context.log.warn(s"invoice purge failed: $cause")
+        // publish a notification when we have purged expired invoices
+        if (expiredPayments.nonEmpty) {
+          context.system.eventStream ! EventStream.Publish(PurgeCompleted)
         }
         waiting()
     }
-
-  private def doPurge(): Unit = {
-    val now = TimestampMilli.now()
-    val expiredPayments = paymentsDb.listExpiredIncomingPayments(0 unixms, now)
-    // purge expired payments
-    expiredPayments.foreach(p => paymentsDb.removeIncomingPayment(p.invoice.paymentHash))
-
-    // publish a notification when we have purged expired invoices
-    if (expiredPayments.nonEmpty) {
-      context.system.eventStream ! EventStream.Publish(PurgeCompleted)
-    }
-
-    // purge complete
-    context.self ! PurgeResult(Success(Done))
-  }
 }
