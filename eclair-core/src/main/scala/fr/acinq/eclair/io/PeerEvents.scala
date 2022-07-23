d68ea52177c5e927f94317e38c7789501b2c857a
DerEwige <reto.seeholzer@freesurf.ch>
2022-07-23 17:32:41 +0200

    pong event created and published
    create pong event and publish pongs, so plugins can subscribe to the latency of the peers

--- eclair-core/src/main/scala/fr/acinq/eclair/io/PeerEvents.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/io/PeerEvents.scala
@@ -31,4 +31,6 @@ case class PeerDisconnected(peer: ActorRef, nodeId: PublicKey) extends PeerEvent
 
 case class LastChannelClosed(peer: ActorRef, nodeId: PublicKey) extends PeerEvent
 
+case class receivedPong(latency: FiniteDuration, nodeId: PublicKey) extends PeerEvent
+
 case class UnknownMessageReceived(peer: ActorRef, nodeId: PublicKey, message: UnknownMessage, connectionInfo: ConnectionInfo) extends PeerEvent
