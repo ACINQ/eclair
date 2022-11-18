69be6bad55f0502b4952121a1c0b6815e93ae34d
Richard Myers <remyers@yakshaver.org>
2022-11-18 11:27:10 +0100

    Add ability to send a custom lightning messages from a loaded plugin

--- eclair-core/src/main/scala/fr/acinq/eclair/transactions/Transactions.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/transactions/Transactions.scala
@@ -106,7 +106,7 @@ object Transactions {
     case object Remote extends TxOwner
   }
 
-  sealed trait TransactionWithInputInfo {
+  trait TransactionWithInputInfo {
     def input: InputInfo
     def desc: String
     def tx: Transaction
