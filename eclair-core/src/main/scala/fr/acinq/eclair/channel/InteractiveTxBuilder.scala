2fa206ca0b9b0185081d98e57d95f085eb03f31a
t-bast <bastuc@hotmail.fr>
2022-11-30 12:02:36 +0100

    fixup! Send error instead tx_abort in interactive-tx

--- eclair-core/src/main/scala/fr/acinq/eclair/channel/InteractiveTxBuilder.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/channel/InteractiveTxBuilder.scala
@@ -763,14 +763,12 @@ private class InteractiveTxBuilder(replyTo: ActorRef[InteractiveTxBuilder.Respon
   }
 
   def signFundingTx(completeTx: SharedTransaction, commitments: Commitments): Behavior[Command] = {
-    val shouldSignFirst = if (completeTx.localAmountIn < completeTx.remoteAmountIn) {
-      // The peer with the lowest total of input amount must transmit its `tx_signatures` first.
-      true
-    } else if (completeTx.localAmountIn == completeTx.remoteAmountIn) {
+    val shouldSignFirst = if (completeTx.localAmountIn == completeTx.remoteAmountIn) {
       // When both peers contribute the same amount, the peer with the lowest pubkey must transmit its `tx_signatures` first.
       LexicographicalOrdering.isLessThan(commitments.localParams.nodeId.value, commitments.remoteNodeId.value)
     } else {
-      false
+      // The peer with the lowest total of input amount must transmit its `tx_signatures` first.
+      completeTx.localAmountIn < completeTx.remoteAmountIn
     }
     if (shouldSignFirst) {
       signTx(completeTx, None)
