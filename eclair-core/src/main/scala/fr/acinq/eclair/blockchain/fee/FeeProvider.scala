13cbf0ff9c745d11d1300178215c7d6a0bd816f3
pm47 <pm.padiou@gmail.com>
2023-06-16 16:51:18 +0200

    remove deprecated methods

--- eclair-core/src/main/scala/fr/acinq/eclair/blockchain/fee/FeeProvider.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/blockchain/fee/FeeProvider.scala
@@ -112,39 +112,11 @@ object FeeratePerKw {
  */
 case class FeeratesPerKB(mempoolMinFee: FeeratePerKB, block_1: FeeratePerKB, blocks_2: FeeratePerKB, blocks_6: FeeratePerKB, blocks_12: FeeratePerKB, blocks_36: FeeratePerKB, blocks_72: FeeratePerKB, blocks_144: FeeratePerKB, blocks_1008: FeeratePerKB) {
   require(mempoolMinFee.feerate > 0.sat && block_1.feerate > 0.sat && blocks_2.feerate > 0.sat && blocks_6.feerate > 0.sat && blocks_12.feerate > 0.sat && blocks_36.feerate > 0.sat && blocks_72.feerate > 0.sat && blocks_144.feerate > 0.sat && blocks_1008.feerate > 0.sat, "all feerates must be strictly greater than 0")
-
-  def feePerBlock(target: Int): FeeratePerKB = {
-    require(target > 0)
-    target match {
-      case 1 => block_1
-      case 2 => blocks_2
-      case t if t <= 6 => blocks_6
-      case t if t <= 12 => blocks_12
-      case t if t <= 36 => blocks_36
-      case t if t <= 72 => blocks_72
-      case t if t <= 144 => blocks_144
-      case _ => blocks_1008
-    }
-  }
 }
 
 /** Fee rates in satoshi-per-kilo-weight (1 kw = 1000 weight units). */
 case class FeeratesPerKw(mempoolMinFee: FeeratePerKw, block_1: FeeratePerKw, blocks_2: FeeratePerKw, blocks_6: FeeratePerKw, blocks_12: FeeratePerKw, blocks_36: FeeratePerKw, blocks_72: FeeratePerKw, blocks_144: FeeratePerKw, blocks_1008: FeeratePerKw) {
   require(mempoolMinFee.feerate > 0.sat && block_1.feerate > 0.sat && blocks_2.feerate > 0.sat && blocks_6.feerate > 0.sat && blocks_12.feerate > 0.sat && blocks_36.feerate > 0.sat && blocks_72.feerate > 0.sat && blocks_144.feerate > 0.sat && blocks_1008.feerate > 0.sat, "all feerates must be strictly greater than 0")
-
-  def feePerBlock(target: Int): FeeratePerKw = {
-    require(target > 0)
-    target match {
-      case 1 => block_1
-      case 2 => blocks_2
-      case t if t <= 6 => blocks_6
-      case t if t <= 12 => blocks_12
-      case t if t <= 36 => blocks_36
-      case t if t <= 72 => blocks_72
-      case t if t <= 144 => blocks_144
-      case _ => blocks_1008
-    }
-  }
 }
 
 object FeeratesPerKw {
