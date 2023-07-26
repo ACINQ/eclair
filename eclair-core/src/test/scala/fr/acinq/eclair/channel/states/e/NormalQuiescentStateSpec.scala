be10434dc66b19a5c8c4f28a3c7f99694bbcca51
t-bast <bastien@acinq.fr>
2023-07-26 15:51:20 +0200

    Group tests and remove unused functions
    This commit contains almost no functional changes, we just group together
    and reorder tests, remove unused code and remove the default value for
    `sendInitialStfu` (which makes tests matrixes easier to read).

--- eclair-core/src/test/scala/fr/acinq/eclair/channel/states/e/NormalQuiescentStateSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/channel/states/e/NormalQuiescentStateSpec.scala
@@ -18,7 +18,7 @@ package fr.acinq.eclair.channel.states.e
 
 import akka.actor.typed.scaladsl.adapter.actorRefAdapter
 import akka.testkit.{TestFSMRef, TestProbe}
-import fr.acinq.bitcoin.scalacompat.{SatoshiLong, Script, Transaction}
+import fr.acinq.bitcoin.scalacompat.{SatoshiLong, Script}
 import fr.acinq.eclair.TestConstants.Bob
 import fr.acinq.eclair.blockchain.CurrentBlockHeight
 import fr.acinq.eclair.blockchain.bitcoind.ZmqWatcher._
@@ -26,7 +26,7 @@ import fr.acinq.eclair.blockchain.fee.FeeratePerKw
 import fr.acinq.eclair.channel._
 import fr.acinq.eclair.channel.fsm.Channel
 import fr.acinq.eclair.channel.fund.InteractiveTxBuilder
-import fr.acinq.eclair.channel.publish.TxPublisher.{PublishFinalTx, PublishReplaceableTx, PublishTx}
+import fr.acinq.eclair.channel.publish.TxPublisher.{PublishFinalTx, PublishTx}
 import fr.acinq.eclair.channel.states.{ChannelStateTestsBase, ChannelStateTestsTags}
 import fr.acinq.eclair.io.Peer
 import fr.acinq.eclair.payment.relay.Relayer.RelayForward
@@ -49,6 +49,8 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     val setup = init(tags = tags)
     import setup._
     reachNormal(setup, tags)
+    alice2bob.ignoreMsg { case _: ChannelUpdate => true }
+    bob2alice.ignoreMsg { case _: ChannelUpdate => true }
     awaitCond(alice.stateName == NORMAL)
     awaitCond(bob.stateName == NORMAL)
     withFixture(test.toNoArgTest(setup))
@@ -90,7 +92,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     }
   }
 
-  private def initiateQuiescence(f: FixtureParam, sendInitialStfu: Boolean = true): TestProbe = {
+  private def initiateQuiescence(f: FixtureParam, sendInitialStfu: Boolean): TestProbe = {
     import f._
 
     val sender = TestProbe()
@@ -109,86 +111,6 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     }
 
     sender
-  }
-
-  private def assertPublished(probe: TestProbe, desc: String): Transaction = {
-    val p = probe.expectMsgType[PublishTx]
-    assert(desc == p.desc)
-    p match {
-      case p: PublishFinalTx => p.tx
-      case p: PublishReplaceableTx => p.txInfo.tx
-    }
-  }
-
-  private def sendErrorAndClose(s: TestFSMRef[ChannelState, ChannelData, Channel], s2r: TestProbe, s2blockchain: TestProbe): Unit = {
-    s2r.expectMsgType[Error]
-    assertPublished(s2blockchain, "commit-tx")
-    assertPublished(s2blockchain, "local-main-delayed")
-    s2blockchain.expectMsgType[WatchTxConfirmed]
-    s2blockchain.expectMsgType[WatchTxConfirmed]
-    s2blockchain.expectNoMessage(100 millis)
-    awaitCond(s.stateName == CLOSING)
-  }
-
-  sealed trait SettlementCommandEnum
-  case object FulfillHtlc extends SettlementCommandEnum
-  case object FailHtlc extends SettlementCommandEnum
-  case object FailMalformedHtlc extends SettlementCommandEnum
-
-  private def receiveSettlementCommand(f: FixtureParam, c: SettlementCommandEnum, sendInitialStfu: Boolean, resetConnection: Boolean = false): Unit = {
-    import f._
-
-    val (preimage, add) = addHtlc(10_000 msat, bob, alice, bob2alice, alice2bob)
-    val cmd = c match {
-      case FulfillHtlc => CMD_FULFILL_HTLC(add.id, preimage)
-      case FailHtlc => CMD_FAIL_HTLC(add.id, Left(randomBytes32()))
-      case FailMalformedHtlc => CMD_FAIL_MALFORMED_HTLC(add.id, randomBytes32(), FailureMessageCodecs.BADONION)
-    }
-    crossSign(bob, alice, bob2alice, alice2bob)
-    val sender = initiateQuiescence(f, sendInitialStfu)
-    safeSend(alice, Seq(cmd))
-    // alice will not forward settlement msg to bob while quiescent
-    alice2bob.expectNoMessage(100 millis)
-    if (resetConnection) {
-      // both alice and bob leave quiescence when the channel is disconnected
-      disconnect(f)
-    } else if (sendInitialStfu) {
-      // alice sends splice-init to bob
-      alice2bob.forward(bob)
-      bob2alice.expectMsgType[SpliceAck]
-      // both alice and bob leave quiescence when the splice aborts
-      bob2alice.forward(alice, TxAbort(channelId(bob), hex"deadbeef"))
-      alice2bob.expectMsgType[TxAbort]
-      alice2bob.forward(bob)
-      bob2alice.expectMsgType[TxAbort]
-    } else {
-      // alice sends a warning and disconnects if an error occurs while waiting for stfu from bob
-      alice ! Channel.QuiescenceTimeout(alicePeer.ref)
-      alice2bob.expectMsgType[Warning]
-      alice2bob.forward(bob)
-      // we should disconnect after giving bob time to receive the warning
-      alicePeer.fishForMessage(3 seconds) {
-        case Peer.Disconnect(nodeId, _) if nodeId == alice.stateData.asInstanceOf[DATA_NORMAL].commitments.params.remoteParams.nodeId => true
-        case _ => false
-      }
-      disconnect(f)
-    }
-    if (resetConnection || !sendInitialStfu) {
-      // any failure during quiescence will cause alice to disconnect
-      sender.expectMsgType[RES_FAILURE[_, _]]
-      assert(alice.stateData.asInstanceOf[DATA_NORMAL].spliceStatus == SpliceStatus.NoSplice)
-      reconnect(f)
-    }
-
-    // alice sends pending settlement after quiescence ends
-    eventually {
-      c match {
-        case FulfillHtlc => alice2bob.expectMsgType[UpdateFulfillHtlc]
-        case FailHtlc => alice2bob.expectMsgType[UpdateFailHtlc]
-        case FailMalformedHtlc => alice2bob.expectMsgType[UpdateFailMalformedHtlc]
-      }
-    }
-    alice2bob.forward(bob)
   }
 
   test("recv stfu when there are pending local changes") { f =>
@@ -210,14 +132,32 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     bob2alice.forward(alice)
   }
 
-  test("recv forbidden commands while quiescent") { f =>
+  test("recv forbidden non-settlement commands while initiator awaiting stfu from remote") { f =>
+    import f._
+    // initiator should reject commands that change the commitment once it became quiescent
+    val sender1, sender2, sender3 = TestProbe()
+    val cmds = Seq(
+      CMD_ADD_HTLC(sender1.ref, 1_000_000 msat, randomBytes32(), CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), TestConstants.emptyOnionPacket, None, localOrigin(sender1.ref)),
+      CMD_UPDATE_FEE(FeeratePerKw(100 sat), replyTo_opt = Some(sender2.ref)),
+      CMD_CLOSE(sender3.ref, None, None)
+    )
+    initiateQuiescence(f, sendInitialStfu = false)
+    safeSend(alice, cmds)
+    sender1.expectMsgType[RES_ADD_FAILED[ForbiddenDuringSplice]]
+    sender2.expectMsgType[RES_FAILURE[CMD_UPDATE_FEE, ForbiddenDuringSplice]]
+    sender3.expectMsgType[RES_FAILURE[CMD_CLOSE, ForbiddenDuringSplice]]
+  }
+
+  test("recv forbidden non-settlement commands while quiescent") { f =>
     import f._
     // both should reject commands that change the commitment while quiescent
     val sender1, sender2, sender3 = TestProbe()
-    val cmds = Seq(CMD_ADD_HTLC(sender1.ref, 1_000_000 msat, randomBytes32(), CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), TestConstants.emptyOnionPacket, None, localOrigin(sender1.ref)),
+    val cmds = Seq(
+      CMD_ADD_HTLC(sender1.ref, 1_000_000 msat, randomBytes32(), CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), TestConstants.emptyOnionPacket, None, localOrigin(sender1.ref)),
       CMD_UPDATE_FEE(FeeratePerKw(100 sat), replyTo_opt = Some(sender2.ref)),
-      CMD_CLOSE(sender3.ref, None, None))
-    initiateQuiescence(f)
+      CMD_CLOSE(sender3.ref, None, None)
+    )
+    initiateQuiescence(f, sendInitialStfu = true)
     safeSend(alice, cmds)
     sender1.expectMsgType[RES_ADD_FAILED[ForbiddenDuringSplice]]
     sender2.expectMsgType[RES_FAILURE[CMD_UPDATE_FEE, ForbiddenDuringSplice]]
@@ -228,51 +168,92 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     sender3.expectMsgType[RES_FAILURE[CMD_CLOSE, ForbiddenDuringSplice]]
   }
 
-  test("recv fulfill htlc command while initiator awaiting stfu from remote") { f =>
+  // @formatter:off
+  sealed trait SettlementCommandEnum
+  case object FulfillHtlc extends SettlementCommandEnum
+  case object FailHtlc extends SettlementCommandEnum
+  // @formatter:on
+
+  private def receiveSettlementCommand(f: FixtureParam, c: SettlementCommandEnum, sendInitialStfu: Boolean, resetConnection: Boolean = false): Unit = {
+    import f._
+
+    val (preimage, add) = addHtlc(10_000 msat, bob, alice, bob2alice, alice2bob)
+    val cmd = c match {
+      case FulfillHtlc => CMD_FULFILL_HTLC(add.id, preimage)
+      case FailHtlc => CMD_FAIL_HTLC(add.id, Left(randomBytes32()))
+    }
+    crossSign(bob, alice, bob2alice, alice2bob)
+    val sender = initiateQuiescence(f, sendInitialStfu)
+    safeSend(alice, Seq(cmd))
+    // alice will not forward settlement msg to bob while quiescent
+    alice2bob.expectNoMessage(100 millis)
+    if (resetConnection) {
+      // both alice and bob leave quiescence when the channel is disconnected
+      disconnect(f)
+    } else if (sendInitialStfu) {
+      // alice sends splice-init to bob
+      alice2bob.forward(bob)
+      bob2alice.expectMsgType[SpliceAck]
+      // both alice and bob leave quiescence when the splice aborts
+      bob2alice.forward(alice, TxAbort(channelId(bob), hex"deadbeef"))
+      alice2bob.expectMsgType[TxAbort]
+      alice2bob.forward(bob)
+      bob2alice.expectMsgType[TxAbort]
+    } else {
+      // alice sends a warning and disconnects if bob doesn't send stfu before the timeout
+      alice ! Channel.QuiescenceTimeout(alicePeer.ref)
+      alice2bob.expectMsgType[Warning]
+      alice2bob.forward(bob)
+      // we should disconnect after giving bob time to receive the warning
+      alicePeer.fishForMessage(3 seconds) {
+        case Peer.Disconnect(nodeId, _) if nodeId == alice.underlyingActor.remoteNodeId => true
+        case _ => false
+      }
+      disconnect(f)
+    }
+    if (resetConnection || !sendInitialStfu) {
+      // any failure during quiescence will cause alice to disconnect
+      sender.expectMsgType[RES_FAILURE[_, _]]
+      assert(alice.stateData.asInstanceOf[DATA_NORMAL].spliceStatus == SpliceStatus.NoSplice)
+      reconnect(f)
+    }
+    // alice sends pending settlement after quiescence ends
+    c match {
+      case FulfillHtlc => alice2bob.expectMsgType[UpdateFulfillHtlc]
+      case FailHtlc => alice2bob.expectMsgType[UpdateFailHtlc]
+    }
+  }
+
+  test("recv CMD_FULFILL_HTLC while initiator awaiting stfu from remote") { f =>
     receiveSettlementCommand(f, FulfillHtlc, sendInitialStfu = false)
   }
 
-  test("recv fail htlc command while initiator awaiting stfu from remote") { f =>
+  test("recv CMD_FAIL_HTLC while initiator awaiting stfu from remote") { f =>
     receiveSettlementCommand(f, FailHtlc, sendInitialStfu = false)
   }
 
-  test("recv fulfill htlc command while initiator awaiting stfu from remote and channel disconnects") { f =>
+  test("recv CMD_FULFILL_HTLC while initiator awaiting stfu from remote and channel disconnects") { f =>
     receiveSettlementCommand(f, FulfillHtlc, sendInitialStfu = false, resetConnection = true)
   }
 
-  test("recv fail htlc command while initiator awaiting stfu from remote and channel disconnects") { f =>
+  test("recv CMD_FAIL_HTLC while initiator awaiting stfu from remote and channel disconnects") { f =>
     receiveSettlementCommand(f, FailHtlc, sendInitialStfu = false, resetConnection = true)
   }
 
-  test("recv fulfill htlc command while quiescent") { f =>
+  test("recv CMD_FULFILL_HTLC while quiescent") { f =>
     receiveSettlementCommand(f, FulfillHtlc, sendInitialStfu = true)
   }
 
-  test("recv fail htlc command while quiescent") { f =>
+  test("recv CMD_FAIL_HTLC while quiescent") { f =>
     receiveSettlementCommand(f, FailHtlc, sendInitialStfu = true)
   }
 
-  test("recv fulfill htlc command while quiescent and channel disconnects") { f =>
+  test("recv CMD_FULFILL_HTLC while quiescent and channel disconnects") { f =>
     receiveSettlementCommand(f, FulfillHtlc, sendInitialStfu = true, resetConnection = true)
   }
 
-  test("recv fail htlc command  while quiescent and channel disconnects") { f =>
+  test("recv CMD_FAIL_HTLC while quiescent and channel disconnects") { f =>
     receiveSettlementCommand(f, FailHtlc, sendInitialStfu = true, resetConnection = true)
-  }
-
-  test("recv settlement commands while initiator awaiting stfu from remote") { f =>
-    import f._
-
-    // initiator should reject commands that change the commitment until the splice is complete
-    val sender1, sender2, sender3 = TestProbe()
-    val cmds = Seq(CMD_ADD_HTLC(sender1.ref, 1_000_000 msat, randomBytes32(), CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), TestConstants.emptyOnionPacket, None, localOrigin(sender1.ref)),
-      CMD_UPDATE_FEE(FeeratePerKw(100 sat), replyTo_opt = Some(sender2.ref)),
-      CMD_CLOSE(sender3.ref, None, None))
-    initiateQuiescence(f, sendInitialStfu = false)
-    safeSend(alice, cmds)
-    sender1.expectMsgType[RES_ADD_FAILED[ForbiddenDuringSplice]]
-    sender2.expectMsgType[RES_FAILURE[CMD_UPDATE_FEE, ForbiddenDuringSplice]]
-    sender3.expectMsgType[RES_FAILURE[CMD_CLOSE, ForbiddenDuringSplice]]
   }
 
   test("recv second stfu while non-initiator waiting for local commitment to be signed") { f =>
@@ -297,7 +278,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
 
   test("recv (forbidden) Shutdown message while quiescent") { f =>
     import f._
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
     val bobData = bob.stateData.asInstanceOf[DATA_NORMAL]
     val forbiddenMsg = Shutdown(channelId(bob), bob.underlyingActor.getOrGenerateFinalScriptPubKey(bobData))
     // both parties will respond to a forbidden msg while quiescent with a warning (and disconnect)
@@ -311,7 +292,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     import f._
     val (preimage, add) = addHtlc(10_000 msat, bob, alice, bob2alice, alice2bob)
     crossSign(bob, alice, bob2alice, alice2bob)
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
     val forbiddenMsg = UpdateFulfillHtlc(channelId(bob), add.id, preimage)
     // both parties will respond to a forbidden msg while quiescent with a warning (and disconnect)
     bob2alice.forward(alice, forbiddenMsg)
@@ -326,7 +307,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     import f._
     val (_, add) = addHtlc(10_000 msat, bob, alice, bob2alice, alice2bob)
     crossSign(bob, alice, bob2alice, alice2bob)
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
     val forbiddenMsg = UpdateFailHtlc(channelId(bob), add.id, randomBytes32())
     // both parties will respond to a forbidden msg while quiescent with a warning (and disconnect)
     bob2alice.forward(alice, forbiddenMsg)
@@ -339,7 +320,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     import f._
     val (_, _) = addHtlc(10_000 msat, bob, alice, bob2alice, alice2bob)
     crossSign(bob, alice, bob2alice, alice2bob)
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
     val forbiddenMsg = UpdateFee(channelId(bob), FeeratePerKw(1 sat))
     // both parties will respond to a forbidden msg while quiescent with a warning (and disconnect)
     bob2alice.forward(alice, forbiddenMsg)
@@ -350,7 +331,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
 
   test("recv (forbidden) UpdateAddHtlc message while quiescent") { f =>
     import f._
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
 
     // have to build a htlc manually because eclair would refuse to accept this command as it's forbidden
     val forbiddenMsg = UpdateAddHtlc(channelId = randomBytes32(), id = 5656, amountMsat = 50000000 msat, cltvExpiry = CltvExpiryDelta(144).toCltvExpiry(currentBlockHeight), paymentHash = randomBytes32(), onionRoutingPacket = TestConstants.emptyOnionPacket, blinding_opt = None)
@@ -428,7 +409,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
     import f._
     val (preimage, add) = addHtlc(50_000_000 msat, alice, bob, alice2bob, bob2alice)
     crossSign(alice, bob, alice2bob, bob2alice)
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
 
     // bob receives the fulfill for htlc, which is ignored because the channel is quiescent
     val fulfillHtlc = CMD_FULFILL_HTLC(add.id, preimage)
@@ -466,7 +447,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
 
   test("receive quiescence timeout while splice in progress") { f =>
     import f._
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
 
     // alice sends splice-init to bob, bob responds with splice-ack
     alice2bob.forward(bob)
@@ -480,7 +461,7 @@ class NormalQuiescentStateSpec extends TestKitBaseClass with FixtureAnyFunSuiteL
 
   test("receive quiescence timeout while splice is waiting for tx_abort from peer") { f =>
     import f._
-    initiateQuiescence(f)
+    initiateQuiescence(f, sendInitialStfu = true)
 
     // bob sends tx-abort to alice but does not receive a tx-abort back from alice
     alice2bob.forward(bob)
