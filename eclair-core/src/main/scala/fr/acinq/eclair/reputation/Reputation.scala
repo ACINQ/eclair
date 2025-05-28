7f8b984b74f4aa91319d6be62f112b268c78999b
Thomas HUET <thomas.huet@acinq.fr>
2025-05-28 11:21:59 +0200

    Use lower endorsement payments to increase confidence of higher endorsement payments

--- eclair-core/src/main/scala/fr/acinq/eclair/reputation/Reputation.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/reputation/Reputation.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 ACINQ SAS
+ * Copyright 2025 ACINQ SAS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,76 +17,95 @@
 package fr.acinq.eclair.reputation
 
 import fr.acinq.bitcoin.scalacompat.ByteVector32
-import fr.acinq.eclair.reputation.Reputation.HtlcId
 import fr.acinq.eclair.{MilliSatoshi, TimestampMilli}
 
+import scala.collection.mutable
 import scala.concurrent.duration.FiniteDuration
 
 /**
- * Created by thomash on 21/07/2023.
+ *
+ * @param weight           How much fees we would have collected in the past if all payments had succeeded (exponential moving average).
+ * @param score            How much fees we have collected in the past (exponential moving average).
+ * @param lastSettlementAt Timestamp of the last recorded payment settlement.
  */
+case class PastScore(weight: Double, score: Double, lastSettlementAt: TimestampMilli)
+
+/** We're relaying that payment and are waiting for it to settle. */
+case class PendingPayment(fee: MilliSatoshi, endorsement: Int, startedAt: TimestampMilli) {
+  def weight(now: TimestampMilli, minDuration: FiniteDuration, multiplier: Double): Double = {
+    val duration = now - startedAt
+    fee.toLong.toDouble * (duration / minDuration).max(multiplier)
+  }
+}
+
+case class HtlcId(channelId: ByteVector32, id: Long)
 
 /**
- * Local reputation for a given incoming node, that should be tracked for each incoming endorsement level.
+ * Local reputation for a given node.
  *
- * @param pastWeight        How much fees we would have collected in the past if all payments had succeeded (exponential moving average).
- * @param pastScore         How much fees we have collected in the past (exponential moving average).
- * @param lastSettlementAt  Timestamp of the last recorded payment settlement.
  * @param pending           Set of pending payments (payments may contain multiple HTLCs when using trampoline).
  * @param halfLife          Half life for the exponential moving average.
  * @param maxRelayDuration  Duration after which payments are penalized for staying pending too long.
  * @param pendingMultiplier How much to penalize pending payments.
  */
-case class Reputation(pastWeight: Double, pastScore: Double, lastSettlementAt: TimestampMilli, pending: Map[HtlcId, Reputation.PendingPayment], halfLife: FiniteDuration, maxRelayDuration: FiniteDuration, pendingMultiplier: Double) {
-  private def decay(now: TimestampMilli): Double = scala.math.pow(0.5, (now - lastSettlementAt) / halfLife)
-
-  private def pendingWeight(now: TimestampMilli): Double = pending.values.map(_.weight(now, maxRelayDuration, pendingMultiplier)).sum
+case class Reputation(pastScores: Array[PastScore], pending: mutable.Map[HtlcId, PendingPayment], halfLife: FiniteDuration, maxRelayDuration: FiniteDuration, pendingMultiplier: Double) {
+  private def decay(now: TimestampMilli, lastSettlementAt: TimestampMilli): Double = scala.math.pow(0.5, (now - lastSettlementAt) / halfLife)
 
   /**
    * Estimate the confidence that a payment will succeed.
    */
-  def getConfidence(fee: MilliSatoshi, now: TimestampMilli = TimestampMilli.now()): Double = {
-    val d = decay(now)
-    d * pastScore / (d * pastWeight + pendingWeight(now) + fee.toLong.toDouble * pendingMultiplier)
+  def getConfidence(fee: MilliSatoshi, endorsement: Int, now: TimestampMilli = TimestampMilli.now()): Double = {
+    val weights = Array.fill(Reputation.endorsementLevels)(0.0)
+    val scores = Array.fill(Reputation.endorsementLevels)(0.0)
+    for (e <- 0 until Reputation.endorsementLevels) {
+      val d = decay(now, pastScores(e).lastSettlementAt)
+      weights(e) += d * pastScores(e).weight
+      scores(e) += d * pastScores(e).score
+    }
+    for (p <- pending.values) {
+      weights(p.endorsement) += p.weight(now, maxRelayDuration, pendingMultiplier)
+    }
+    weights(endorsement) += fee.toLong.toDouble * pendingMultiplier
+    /*
+     Higher endorsement buckets may have fewer payments which makes the weight of pending payments disproportionately
+     important. To counter this effect, we try adding payments from the lower buckets to see if it gives us a higher
+     confidence score.
+     It is acceptable to use payments with lower endorsements to increase the confidence score but not payments with
+     higher endorsements.
+    */
+    var score = scores(endorsement)
+    var weight = weights(endorsement)
+    var confidence = score / weight
+    for (e <- Range.inclusive(endorsement - 1, 0, step = -1)) {
+      score += scores(e)
+      weight += weights(e)
+      confidence = confidence.max(score / weight)
+    }
+    confidence
   }
 
   /**
    * Register a pending relay.
-   *
-   * @return updated reputation
    */
-  def attempt(htlcId: HtlcId, fee: MilliSatoshi, now: TimestampMilli = TimestampMilli.now()): Reputation =
-    copy(pending = pending + (htlcId -> Reputation.PendingPayment(fee, now)))
+  def attempt(htlcId: HtlcId, fee: MilliSatoshi, endorsement: Int, now: TimestampMilli = TimestampMilli.now()): Unit =
+    pending(htlcId) = PendingPayment(fee, endorsement, now)
 
   /**
    * When a payment is settled, we record whether it succeeded and how long it took.
-   *
-   * @return updated reputation
    */
-  def record(htlcId: HtlcId, isSuccess: Boolean, now: TimestampMilli = TimestampMilli.now()): Reputation = {
-    pending.get(htlcId) match {
-      case Some(p) =>
-        val d = decay(now)
-        val newWeight = d * pastWeight + p.weight(now, maxRelayDuration, if (isSuccess) 1.0 else 0.0)
-        val newScore = d * pastScore + (if (isSuccess) p.fee.toLong.toDouble else 0)
-        Reputation(newWeight, newScore, now, pending - htlcId, halfLife, maxRelayDuration, pendingMultiplier)
-      case None => this
-    }
-  }
+  def record(htlcId: HtlcId, isSuccess: Boolean, now: TimestampMilli = TimestampMilli.now()): Unit =
+    pending.remove(htlcId).foreach(p => {
+      val d = decay(now, pastScores(p.endorsement).lastSettlementAt)
+      val newWeight = d * pastScores(p.endorsement).weight + p.weight(now, maxRelayDuration, if (isSuccess) 1.0 else 0.0)
+      val newScore = d * pastScores(p.endorsement).score + (if (isSuccess) p.fee.toLong.toDouble else 0)
+      pastScores(p.endorsement) = PastScore(newWeight, newScore, now)
+    })
 }
 
 object Reputation {
-  case class HtlcId(channelId: ByteVector32, id: Long)
-
-  /** We're relaying that payment and are waiting for it to settle. */
-  case class PendingPayment(fee: MilliSatoshi, startedAt: TimestampMilli) {
-    def weight(now: TimestampMilli, minDuration: FiniteDuration, multiplier: Double): Double = {
-      val duration = now - startedAt
-      fee.toLong.toDouble * (duration / minDuration).max(multiplier)
-    }
-  }
+  val endorsementLevels = 8
 
   case class Config(enabled: Boolean, halfLife: FiniteDuration, maxRelayDuration: FiniteDuration, pendingMultiplier: Double)
 
-  def init(config: Config): Reputation = Reputation(0.0, 0.0, TimestampMilli.min, Map.empty, config.halfLife, config.maxRelayDuration, config.pendingMultiplier)
+  def init(config: Config): Reputation = Reputation(Array.fill(endorsementLevels)(PastScore(0.0, 0.0, TimestampMilli.min)), mutable.HashMap.empty, config.halfLife, config.maxRelayDuration, config.pendingMultiplier)
 }
