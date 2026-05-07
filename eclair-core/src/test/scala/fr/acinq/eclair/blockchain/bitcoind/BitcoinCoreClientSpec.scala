c350f3bc146a79f08139d2da8c0faaf895883556
sstone <fabrice.drouin@acinq.fr>
2026-05-07 13:58:51 +0200

    Make use of txospender mandatory, remove block scanning code
    On startup, eclair will check that both txindex and txospenderindex are enabled and synced, and will stop if they're not.
    Block scanning code used to find spending transactions is removed, we just use the txospenderindex now.

--- eclair-core/src/test/scala/fr/acinq/eclair/blockchain/bitcoind/BitcoinCoreClientSpec.scala
+++ eclair-core/src/test/scala/fr/acinq/eclair/blockchain/bitcoind/BitcoinCoreClientSpec.scala
@@ -1745,6 +1745,8 @@ class BitcoinCoreClientSpec extends TestKitBaseClass with BitcoindService with A
     generateBlocks(1)
     bitcoinClient.getBlockHeight().pipeTo(sender.ref)
     val blockHeight1 = sender.expectMsgType[BlockHeight]
+    bitcoinClient.getBlockId(blockHeight1.toInt).pipeTo(sender.ref)
+    val blockId = sender.expectMsgType[BlockId]
     assert(blockHeight1 == blockHeight + 1)
     bitcoinClient.getTxConfirmations(tx1.txid).pipeTo(sender.ref)
     sender.expectMsg(Some(1))
@@ -1756,10 +1758,8 @@ class BitcoinCoreClientSpec extends TestKitBaseClass with BitcoindService with A
     generateBlocks(10)
     bitcoinClient.lookForMempoolSpendingTx(tx1.txIn.head.outPoint).pipeTo(sender.ref)
     sender.expectMsgType[Failure]
-    bitcoinClient.lookForSpendingTx(None, tx1.txIn.head.outPoint, limit = 5).pipeTo(sender.ref)
-    sender.expectMsgType[Failure]
-    bitcoinClient.lookForSpendingTx(None, tx1.txIn.head.outPoint, limit = 15).pipeTo(sender.ref)
-    sender.expectMsg(tx1)
+    bitcoinClient.findSpendingTx(tx1.txIn.head.outPoint).pipeTo(sender.ref)
+    sender.expectMsg(Some(tx1, Some(blockId)))
   }
 
   test("get index information") {
