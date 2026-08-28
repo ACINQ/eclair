88102e1daac65a9a96c284811f0ab4a9cad22c83
t-bast <bastien@acinq.fr>
2026-08-28 17:13:21 +0200

    Introduce `min-local-trampoline-fees`
    Another approach, slightly different from the previous commit, where
    we instead set a floor for trampoline fees that may be different from
    our local channel fees when trampoline payments contain a routing
    hint.

--- eclair-core/src/main/scala/fr/acinq/eclair/payment/relay/Relayer.scala
+++ eclair-core/src/main/scala/fr/acinq/eclair/payment/relay/Relayer.scala
@@ -145,7 +145,7 @@ object Relayer extends Logging {
   case class RelayParams(publicChannelFees: RelayFees,
                          privateChannelFees: RelayFees,
                          minTrampolineFees: RelayFees,
-                         trampolineIgnoreLocalFees: Boolean,
+                         minLocalTrampolineFees: RelayFees,
                          resetExistingChannels: Boolean,
                          enforcementDelay: FiniteDuration,
                          asyncPaymentsParams: AsyncPaymentsParams,
